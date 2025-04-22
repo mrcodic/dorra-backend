7bc5c01dc10cc7a78f024b48ab2de92157b4d748
yomna <yomnaali463@gmail.com>
2025-04-22 13:51:51 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -46,14 +46,17 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+
             'stream' => [
                 'ssl' => [
                     'allow_self_signed' => true,
                     'verify_peer' => false,
                     'verify_peer_name' => false,
                 ],
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            ],
         ],
+
 
         'ses' => [
             'transport' => 'ses',
