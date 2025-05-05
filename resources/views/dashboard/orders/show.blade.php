c26540e14bcd23c4c6cd3a7a3513b25b4b1145c0
mariamRaslan <mariamraslan231@gmail.com>
2025-05-05 14:40:43 +0300

    edit and show order pages layout

--- resources/views/dashboard/orders/show.blade.php
+++ resources/views/dashboard/orders/show.blade.php
@@ -1,64 +1,157 @@
 @extends('layouts/contentLayoutMaster')
-
-@section('title', 'Show Product')
-@section('main-page', 'Products')
-@section('sub-page', 'Show Product')
+@section('title', 'Show orderss')
+@section('main-page', 'Orders')
+@section('sub-page', 'Show Orders')
 
 @section('vendor-style')
-    {{-- Page Css files --}}
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/animate/animate.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/sweetalert2.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset('vendors/fonts/fontawesome.css') }}">
-@endsection
-
-@section('page-style')
-    {{-- Page Css files --}}
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/extensions/ext-component-sweet-alerts.css')) }}">
+<!-- Vendor CSS Files -->
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
 @endsection
 
 @section('content')
+<div class="container bg-white rounded-3 p-3">
+    <div class="d-flex align-items-center justify-content-between mb-3">
+        <div><span class="fs-16 text-dark fw-bold">Order Number: </span><span class="fs-4 text-black fw-bold">#1234567</span></div>
+        <div class="d-flex align-items-center status-pill justify-content-center">
+            <div class="status-icon me-1">
+                <i data-feather="check"></i>
+            </div>
+            <span class="status-text">Confirmed</span>
+        </div>
+    </div>
+    <form>
+        <div class="row">
+            <!-- Left Column -->
+            <div class="col-12 col-md-4">
+                <h5 class="mb-2 fs-16 text-black">Customer Details</h5>
+                <div class="mb-2">
+                    <label class="form-label fw-bold">Name</label>
+                    <input type="text" class="form-control" name="name" value="John Doe" disabled>
+                </div>
+                <div class="mb-2">
+                    <label class="form-label fw-bold">Email</label>
+                    <input type="email" class="form-control" name="email" value="john@example.com" disabled>
+                </div>
+                <div class="mb-4">
+                    <label class="form-label fw-bold">Phone</label>
+                    <input type="text" class="form-control" name="phone" value="+1 123 456 7890" disabled>
+                </div>
 
+                <span class="text-black fs-16 fw-bold mb-1">Address:</span>
+                <div class="border rounded p-2 mb-2 text-black fs-5">
+                    1234 Example St, Suite 100<br>City, Country
+                </div>
+                <span class="text-black fs-16 fw-bold mb-1">Delivery Instructions:</span>
+                <div class="border rounded p-1 mb-2 text-black fs-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
 
+                <div class="rounded p-1 d-flex align-items-center text-black" style="background-color: #FCF8FC;">
+                    <i data-feather="truck" class="me-2"></i>
+                    <div>
+                        <p class="fs-4">Estimated delivery time</p>
+                        <div class="fs-16">Tomorrow, 2:00 PM - 4:00 PM</div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Right Column -->
+            <div class="col-12 col-md-8">
+                <div class="p-1 rounded" style="background-color: #FCF8FC;">
+                    <p class="mb-1 fs-16 text-dark">Order Placed on:</p>
+                    <p class="fs-16 text-black">April 30, 2025</p>
+                </div>
+                <label class="form-label fw-bold mt-3 mb-1 fs-16 text-black">Payment Status</label>
+                <p >Completed</p>
+
+                <h5 class="fw-bold mt-3 mb-1 fs-16 text-black">Items</h5>
+                <div class="mb-1">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="d-flex">
+                            <img src="{{ asset('images/banner/banner-1.jpg') }}" class="mx-1 rounded" alt="Product" style="width: 60px; height: 60px;">
+                            <div>
+                                <div class="fw-bold text-black fs-16">Product Name 1</div>
+                                <div class="text-dark fs-5">Qty: 2</div>
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <div class="fw-bold text-black">$40.00</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3 border-bottom pb-2">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="d-flex">
+                            <img src="{{ asset('images/banner/banner-1.jpg') }}" class="mx-1 rounded" alt="Product" style="width: 60px; height: 60px;">
+                            <div>
+                                <div class="fw-bold text-black fs-16">Product Name 1</div>
+                                <div class="text-dark fs-5">Qty: 2</div>
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <div class="fw-bold text-black">$40.00</div>
+                        </div>
+                    </div>
+                </div>
+
+                <h5 class="mt-3 mb-1 text-black fs-16">Pricing Details</h5>
+                <div class="d-flex justify-content-between mb-1">
+                    <span class="text-dark fs-16 fw-bold">Subtotal</span>
+                    <span class="fs-4 text-black fw-bold">$65.00</span>
+                </div>
+                <div class="d-flex justify-content-between mb-1">
+                    <span class="text-dark fs-16 fw-bold">Discount</span>
+                    <span class="fs-16 text-black">-$5.00</span>
+                </div>
+                <div class="d-flex justify-content-between mb-1">
+                    <span class="text-dark fs-16 fw-bold">
+                        Delivery
+                        <i data-feather="info" data-bs-toggle="tooltip" title="Delivery charges may vary based on location."></i>
+                    </span>
+                    <span class="fs-16 text-black">$5.00</span>
+                </div>
+                <div class="d-flex justify-content-between mb-1">
+                    <span class="text-dark fs-16 fw-bold">
+                        Tax
+                        <i data-feather="info" data-bs-toggle="tooltip" title="Tax is calculated as per applicable laws."></i>
+                    </span>
+                    <span class="fs-16 text-black">$3.00</span>
+                </div>
+
+                <hr class="border-dashed my-1">
+
+                <div class="d-flex justify-content-between fw-bold fs-5 mb-3">
+                    <span class="fs-4 text-black ">Total</span>
+                    <span class="fs-4 text-black fw-bold">$68.00</span>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label fw-bold">Order Status</label>
+                    <div class="d-flex align-items-center status-pill justify-content-center">
+                        <div class="status-icon me-1">
+                            <i data-feather="check"></i>
+                        </div>
+                        <span class="status-text">Confirmed</span>
+                    </div>
+                </div>
+
+                <div class="d-flex gap-2 justify-content-end ">         
+                        <a class="btn btn-primary" href="/orders/1/edit" >Edit Order</a>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
 @endsection
 
 @section('vendor-script')
-    {{-- Vendor js files --}}
-    <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/cleave/cleave.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/cleave/addons/cleave-phone.us.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
-    {{-- data table --}}
-    <script src="{{ asset(mix('vendors/js/extensions/moment.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap5.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/pdfmake.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/vfs_fonts.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.html5.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.print.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.rowGroup.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/extensions/sweetalert2.all.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/extensions/polyfill.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/forms/repeater/jquery.repeater.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
 @endsection
 
 @section('page-script')
-
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-    {{-- Page js files --}}
-    <script src="{{ asset('js/scripts/pages/modal-edit-user.js') }}?v={{ time() }}"></script>
-    <script src="{{ asset(mix('js/scripts/pages/app-user-view-account.js')) }}"></script>
-    <script src="{{ asset(mix('js/scripts/pages/app-user-view.js')) }}"></script>
-    <script src="{{ asset('js/scripts/ui/star-rate.js') }}?v={{ time() }}"></script>
-   
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+<script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<script src="https://unpkg.com/feather-icons"></script>
 @endsection
---
+++ resources/views/modals/orders/edit-shipping-details.blade.php
@@ -0,0 +1,220 @@
+<div class="modal modal-slide-in new-user-modal fade" id="editOrderShippingModal">
+    <div class="modal-dialog">
+        <div class="add-new-user modal-content pt-0">
+            <form id="editTagForm" enctype="multipart/form-data" action="">
+                @csrf
+                @method("PUT")
+                <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close">×</button>
+                <div class="modal-header mb-1">
+                    <h5 class="modal-title fs-3" id="exampleModalLabel">Edit Shipping Details</h5>
+                </div>
+                <div class="modal-body flex-grow-1">
+                    <input type="hidden" id="edit-tag-id">
+
+                    <!-- Shipping Method Selection -->
+                    <div class="mb-3" id="shippingMethodSection">
+                        <label class="form-label fw-bold fs-5 mb-2">Shipping Method</label>
+                        <div class="d-flex gap-2 ">
+                            <div class="col-6 form-check border rounded-3 p-1 px-3 flex-fill">
+                                <input class="form-check-input" type="radio" name="shipping_method" id="shipToCustomer" value="ship" checked>
+                                <label class="form-check-label fs-4 text-black" for="shipToCustomer">
+                                    Ship to customer
+                                </label>
+                            </div>
+                            <div class="col-6 form-check border rounded-3 p-1 px-3 flex-fill">
+                                <input class="form-check-input" type="radio" name="shipping_method" id="pickUp" value="pickup">
+                                <label class="form-check-label fs-4 text-black" for="pickUp">
+                                    456 Ocean Ave, Miami, FL
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Ship to Customer Section -->
+                    <div id="shipSection">
+                        <!-- Existing Addresses -->
+                        <div class="d-flex gap-2 ">
+                            <div class="col-6 form-check border rounded-3 p-1 px-3 flex-fill">
+                                <input class="form-check-input" type="radio" name="address_id" id="address1" value="1">
+                                <label class="form-check-label fs-4 text-black" for="shipToCustomer">
+
+                                    <p>Home</p>
+                                    <p class="text-dark fs-16">15 street name, neighborhood</p>
+                                </label>
+                            </div>
+                            <div class="col-6 form-check border rounded-3 p-1 px-3 flex-fill">
+                                <input class="form-check-input" type="radio" name="address_id" id="address2" value="2">
+                                <label class="form-check-label fs-4 text-black" for="pickUp">
+                                    <p>Office</p>
+                                    <p class="text-dark fs-16">15 street name, neighborhood</p>
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Divider -->
+                        <div class="text-center my-3 fw-bold">OR</div>
+
+                        <!-- Add New Address -->
+                        <div class="">
+                            <h5 class="mb-2 text-black fs-4">Add new address</h5>
+                            <div class="mb-2">
+                                <label class="form-label ">Address Label</label>
+                                <select class="form-select">
+                                    <option>Home</option>
+                                    <option>Work</option>
+                                    <option>Other</option>
+                                </select>
+                            </div>
+                            <div class="row g-2 mb-2">
+                                <div class="col">
+                                    <label class="form-label ">Country</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col">
+                                    <label class="form-label ">State</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="mb-2">
+                                <label class="form-label">Address Line</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="mb-2">
+                                <label class="form-label">Delivery Instructions</label>
+                                <textarea class="form-control" rows="2"></textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Pick Up Section -->
+                    <div id="pickupSection" style="display: none;">
+                        <div class="d-flex justify-content-between align-items-center mb-2">
+                            <h5 class="form-label ">Pick up Location</h5>
+                            <button type="button" class="lined-btn">Change Location</button>
+                        </div>
+                        <div class="border rounded-3 p-1 mb-2">
+                            <p class="text-black">Office</p>
+                            <p>789 Store Blvd, Los Angeles, CA</p>
+                            <p>Cairo, Egypt</p>
+                        </div>
+
+                        <div class="mb-2">
+                            <label class="form-label fw-bold">Who's picking up the package?</label>
+                        </div>
+                        <div class="row g-2 mb-2">
+                            <div class="col">
+                                <label class="form-label">First Name</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col">
+                                <label class="form-label">Last Name</label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="mb-2">
+                            <label class="form-label">Email</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="mb-2">
+                            <label class="form-label">Phone Number</label>
+                            <input type="tel" class="form-control">
+                        </div>
+                    </div>
+
+                    <!-- Change Location Section -->
+                    <div id="changeLocationSection" style="display: none;">
+                        <div class="d-flex align-items-center gap-1 mb-3">
+                            <i data-feather="chevron-left" class="cursor-pointer" id="backToPickup" style="cursor: pointer;"></i>
+                            <h5 class="fs-4 text-black mb-0">Change Pick up Location</h5>
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" placeholder="Search for a location..." id="locationSearch">
+                        </div>
+
+                        <div id="mapPlaceholder" style="height: 300px; background-color: #f0f0f0; border-radius: 8px;">
+                            <p class="text-center text-muted pt-5">Map will display here based on search</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer border-top-0">
+                    <button type="button" class="btn btn-outline-secondary fs-5" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary fs-5" id="saveChangesButton">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        feather.replace();
+        const shipRadio = document.getElementById("shipToCustomer");
+        const pickupRadio = document.getElementById("pickUp");
+        const shipSection = document.getElementById("shipSection");
+        const pickupSection = document.getElementById("pickupSection");
+
+        function toggleSections() {
+            if (shipRadio.checked) {
+                shipSection.style.display = "block";
+                pickupSection.style.display = "none";
+            } else {
+                shipSection.style.display = "none";
+                pickupSection.style.display = "block";
+            }
+        }
+
+        shipRadio.addEventListener("change", toggleSections);
+        pickupRadio.addEventListener("change", toggleSections);
+        toggleSections();
+    });
+</script>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const shipRadio = document.getElementById("shipToCustomer");
+        const pickupRadio = document.getElementById("pickUp");
+        const shipSection = document.getElementById("shipSection");
+        const pickupSection = document.getElementById("pickupSection");
+        const changeLocationSection = document.getElementById("changeLocationSection");
+        const shippingMethodSection = document.getElementById("shippingMethodSection");
+
+        const changeLocationBtn = pickupSection.querySelector(".lined-btn");
+        const backToPickupBtn = document.getElementById("backToPickup");
+
+        function toggleSections() {
+            if (shipRadio.checked) {
+                shipSection.style.display = "block";
+                pickupSection.style.display = "none";
+                changeLocationSection.style.display = "none";
+                shippingMethodSection.style.display = "block";
+            } else {
+                shipSection.style.display = "none";
+                pickupSection.style.display = "block";
+                changeLocationSection.style.display = "none";
+                shippingMethodSection.style.display = "block";
+            }
+        }
+
+        shipRadio.addEventListener("change", toggleSections);
+        pickupRadio.addEventListener("change", toggleSections);
+        toggleSections();
+
+        changeLocationBtn.addEventListener("click", function() {
+            pickupSection.style.display = "none";
+            changeLocationSection.style.display = "block";
+            shippingMethodSection.style.display = "none";
+        });
+
+        backToPickupBtn.addEventListener("click", function() {
+            changeLocationSection.style.display = "none";
+            pickupSection.style.display = "block";
+            shippingMethodSection.style.display = "block";
+        });
+
+        // Initialize feather icons
+        feather.replace();
+    });
+</script>
+
