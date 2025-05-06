b4ff75b71c13b9bac034bc36afd8528b2de8f870
mariamRaslan <mariamraslan231@gmail.com>
2025-05-06 10:38:09 +0300

    permisions page

--- resources/views/dashboard/permissions/index.blade.php
+++ resources/views/dashboard/permissions/index.blade.php
@@ -1,204 +1,117 @@
 @extends('layouts/contentLayoutMaster')
 
-@section('title', 'Products')
-@section('main-page', 'Products')
+@section('title', 'Permissions')
+@section('main-page', 'Permissions')
 
 @section('vendor-style')
-    {{-- Page Css files --}}
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap5.min.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
+{{-- Page Css files --}}
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')) }}">
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap5.min.css')) }}">
+<link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
 
 
-    <!-- SweetAlert2 CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+<!-- SweetAlert2 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 
-    <!-- SweetAlert2 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!-- SweetAlert2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @endsection
 
 @section('page-style')
-    {{-- Page Css files --}}
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+{{-- Page Css files --}}
+<link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
 @endsection
 
 @section('content')
-    <!-- users list start -->
-    <section class="app-user-list">
-        <!-- list and filter start -->
-        <div class="card">
-            <div class="card-body ">
-                <div class="row">
-                    <div class="col-md-4 user_role"></div>
-                    <div class="col-md-4 user_plan"></div>
-                    <div class="col-md-4 user_status"></div>
-                </div>
+<!-- users list start -->
+<section class="app-user-list">
+    <!-- list and filter start -->
+    <div class="card">
+        <div class="card-body ">
+            <div class="row">
+                <div class="col-md-4 user_role"></div>
+                <div class="col-md-4 user_plan"></div>
+                <div class="col-md-4 user_status"></div>
             </div>
-            <div class="card-datatable table-responsive pt-0">
-                <table class="product-list-table table">
-                    <thead class="table-light">
+        </div>
+        <div class="card-datatable table-responsive pt-0">
+            <div class="d-flex justify-content-between align-items-center gap-1 px-1">
+                <form action="" method="get" class="flex-grow-1 me-1 position-relative" style="max-width: 75%;">
+                    <i data-feather="search" class="position-absolute top-50 translate-middle-y mx-1 text-muted"></i>
+                    <input
+                        type="text"
+                        class="form-control ps-5 border rounded-3"
+                        name="search_value"
+                        id="search-category-form"
+                        placeholder="Search here"
+                        style="height: 38px;">
+                </form>
+
+                <input type="date" class="form-control border rounded-3" style="width: 120px; height: 38px;" />
+
+                <a class="btn btn-outline-primary ms-2" href="{{ route('categories.create') }}">Add New Permission</a>
+            </div>
+            <table class="permisions-list-table table mt-2">
+                <thead class="table-light">
                     <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Tags</th>
-                        <th>NO.of Purchas</th>
-                        <th>Added Date</th>
-                        <th>Rating</th>
+                        <th><input type="checkbox" id="select-all-checkbox"></th>
+                        <th>Permission</th>
+                        <th>Assigned To</th>
+                        <th>Date</th>
                         <th>Actions</th>
                     </tr>
-                    </thead>
-                </table>
-            </div>
-            <!-- Modal to add new user starts-->
-            <div class="modal modal-slide-in new-user-modal fade" id="modals-slide-in">
-                <div class="modal-dialog">
-                    <form class="add-new-user modal-content pt-0">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-                        <div class="modal-header mb-1">
-                            <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
-                        </div>
-                        <div class="modal-body flex-grow-1">
-                            <div class="mb-1">
-                                <label class="form-label" for="basic-icon-default-fullname">Full Name</label>
-                                <input
-                                    type="text"
-                                    class="form-control dt-full-name"
-                                    id="basic-icon-default-fullname"
-                                    placeholder="John Doe"
-                                    name="user-fullname"
-                                />
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="basic-icon-default-uname">Username</label>
-                                <input
-                                    type="text"
-                                    id="basic-icon-default-uname"
-                                    class="form-control dt-uname"
-                                    placeholder="Web Developer"
-                                    name="user-name"
-                                />
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="basic-icon-default-email">Email</label>
-                                <input
-                                    type="text"
-                                    id="basic-icon-default-email"
-                                    class="form-control dt-email"
-                                    placeholder="john.doe@example.com"
-                                    name="user-email"
-                                />
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="basic-icon-default-contact">Contact</label>
-                                <input
-                                    type="text"
-                                    id="basic-icon-default-contact"
-                                    class="form-control dt-contact"
-                                    placeholder="+1 (609) 933-44-22"
-                                    name="user-contact"
-                                />
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="basic-icon-default-company">Company</label>
-                                <input
-                                    type="text"
-                                    id="basic-icon-default-company"
-                                    class="form-control dt-contact"
-                                    placeholder="PIXINVENT"
-                                    name="user-company"
-                                />
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="country">Country</label>
-                                <select id="country" class="select2 form-select">
-                                    <option value="Australia">USA</option>
-                                    <option value="Bangladesh">Bangladesh</option>
-                                    <option value="Belarus">Belarus</option>
-                                    <option value="Brazil">Brazil</option>
-                                    <option value="Canada">Canada</option>
-                                    <option value="China">China</option>
-                                    <option value="France">France</option>
-                                    <option value="Germany">Germany</option>
-                                    <option value="India">India</option>
-                                    <option value="Indonesia">Indonesia</option>
-                                    <option value="Israel">Israel</option>
-                                    <option value="Italy">Italy</option>
-                                    <option value="Japan">Japan</option>
-                                    <option value="Korea">Korea, Republic of</option>
-                                    <option value="Mexico">Mexico</option>
-                                    <option value="Philippines">Philippines</option>
-                                    <option value="Russia">Russian Federation</option>
-                                    <option value="South Africa">South Africa</option>
-                                    <option value="Thailand">Thailand</option>
-                                    <option value="Turkey">Turkey</option>
-                                    <option value="Ukraine">Ukraine</option>
-                                    <option value="United Arab Emirates">United Arab Emirates</option>
-                                    <option value="United Kingdom">United Kingdom</option>
-                                    <option value="United States">United States</option>
-                                </select>
-                            </div>
-                            <div class="mb-1">
-                                <label class="form-label" for="user-role">User Role</label>
-                                <select id="user-role" class="select2 form-select">
-                                    <option value="subscriber">Subscriber</option>
-                                    <option value="editor">Editor</option>
-                                    <option value="maintainer">Maintainer</option>
-                                    <option value="author">Author</option>
-                                    <option value="admin">Admin</option>
-                                </select>
-                            </div>
-                            <div class="mb-2">
-                                <label class="form-label" for="user-plan">Select Plan</label>
-                                <select id="user-plan" class="select2 form-select">
-                                    <option value="basic">Basic</option>
-                                    <option value="enterprise">Enterprise</option>
-                                    <option value="company">Company</option>
-                                    <option value="team">Team</option>
-                                </select>
-                            </div>
-                            <button type="submit" class="btn btn-primary me-1 data-submit">Submit</button>
-                            <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                        </div>
+                </thead>
+            </table>
+            <div id="bulk-delete-container" class="my-2 bulk-delete-container" style="display: none;">
+                <div class="delete-container">
+                    <p id="selected-count-text">0 Categories are selected</p>
+                    <form id="bulk-delete-form" method="POST" action="{{ route('categories.bulk-delete') }}">
+                        @csrf
+                        <button type="submit" id="delete-selected-btn"
+                            class="btn btn-outline-danger d-flex justify-content-center align-items-center gap-1 delete-selected-btns">
+                            <i data-feather="trash-2"></i> Delete Selected
+                        </button>
                     </form>
+
+
                 </div>
             </div>
-            <!-- Modal to add new user Ends-->
         </div>
-        <!-- list and filter end -->
-    </section>
-    <!-- users list ends -->
+
+    </div>
+    <!-- list and filter end -->
+</section>
+<!-- users list ends -->
 @endsection
 
 @section('vendor-script')
-    {{-- Vendor js files --}}
-    <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap5.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/pdfmake.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/vfs_fonts.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.html5.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.print.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.rowGroup.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/cleave/cleave.min.js')) }}"></script>
-    <script src="{{ asset(mix('vendors/js/forms/cleave/addons/cleave-phone.us.js')) }}"></script>
+{{-- Vendor js files --}}
+<script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap5.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/jszip.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/pdfmake.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/vfs_fonts.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/buttons.html5.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/buttons.print.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.rowGroup.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/forms/cleave/cleave.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/forms/cleave/addons/cleave-phone.us.js')) }}"></script>
 @endsection
 
 @section('page-script')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-   <script>
-       {{--const productsDataUrl = "{{ route('products.data') }}";--}}
-       {{--const productsCreateUrl = "{{ route('products.create') }}";--}}
-   </script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+<script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<script>
 
-    {{-- Page js files --}}
-{{--    <script src="{{ asset('js/scripts/pages/app-product-list.js') }}?v={{ time() }}"></script>--}}
+</script>
+
+{{-- Page js files --}}
+{{-- <script src="{{ asset('js/scripts/pages/app-permisions-list.js') }}?v={{ time() }}"></script>--}}
 @endsection
