98a5ebf68d6459aa1f1fc398e5708999d21098c6
mariamRaslan <mariamraslan231@gmail.com>
2025-05-26 12:16:07 +0300

    edit sidebar icons

--- resources/views/panels/submenu.blade.php
+++ resources/views/panels/submenu.blade.php
@@ -11,7 +11,7 @@
         <a href="{{ isset($submenu->url) ? url($submenu->url) : 'javascript:void(0)' }}" class="d-flex align-items-center"
           target="{{ isset($submenu->newTab) && $submenu->newTab === true ? '_blank' : '_self' }}">
           @if (isset($submenu->icon))
-            <i data-feather="{{ $submenu->icon }}"></i>
+            <i data-feather="{{ $submenu->icon }}" style="visibility: hidden;" ></i>
           @endif
           <span class="menu-item text-truncate">{{ __('locale.' . $submenu->name) }}</span>
         </a>
