1ff5293a44dbe252da43d083ab4d63c816378c6d
yomna ali <yomnaali463@gmail.com>
2025-05-31 14:44:57 +0300

    asset library

--- app/Http/Controllers/Shared/LibraryAssetController.php
+++ app/Http/Controllers/Shared/LibraryAssetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Shared;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MediaResource;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -20,14 +21,22 @@ class LibraryAssetController extends Controller
 
     public function index()
    {
-       $media = Media::query()->whereMorphedTo('model',auth($this->activeGuard)->user())
+//       $media = Media::query()->whereMorphedTo('model',auth($this->activeGuard)->user())
+//           ->whereCollectionName("{$this->activeGuard}_assets")
+//           ->get();
+     $media = Media::query()->whereMorphedTo('model',Admin::find(1))
+           ->whereCollectionName("web_assets")
            ->get();
        return Response::api(data: MediaResource::collection($media));
    }
 
     public function store(Request $request)
     {
-        $media = handleMediaUploads($request->file('file'),auth($this->activeGuard)->user(),"$this->activeGuard.'_assets'");
+        $request->validate([
+            'file' => ['required','file']
+        ]);
+//        $media = handleMediaUploads($request->file('file'),auth($this->activeGuard)->user(),"{$this->activeGuard}_assets");
+        $media = handleMediaUploads($request->file('file'),Admin::find(1),"web_assets");
         return Response::api(data: MediaResource::make($media));
 
    }
