a13887bec444111c9e0a1d98bff5278a4124de9a
yomna <yomnaali463@gmail.com>
2025-06-23 14:52:10 +0300

    Update DesignResource.php

--- app/Http/Resources/Design/DesignResource.php
+++ app/Http/Resources/Design/DesignResource.php
@@ -23,8 +23,7 @@ class DesignResource extends JsonResource
             'current_version' => $this->current_version,
             'product' => ProductResource::make($this->whenLoaded('product')),
             'template' => TemplateResource::make($this->whenLoaded('template')),
-            'placed_on' => $this->whenLoaded('cartItems',function (){
-            }),
+            'placed_on' => optional($this->pivot)->created_at->format('d/m/Y'),
             'price' => $this->total_price,
             'quantity' => $this->productPrice?->quantity ?? $this->quantity,
         ];
