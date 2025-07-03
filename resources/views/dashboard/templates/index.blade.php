7ebe68f6f912380311e5b097ccf12979aa8c19bd
yomna <yomnaali463@gmail.com>
2025-07-03 17:08:49 +0300

    template

--- resources/views/dashboard/templates/index.blade.php
+++ resources/views/dashboard/templates/index.blade.php
@@ -113,7 +113,7 @@
                                 <i data-feather="upload"></i>
                                 Upload Template
                             </a>
-                            <a class="btn btn-primary w-100 w-md-auto" href="{{ route('product-templates.create') }}">
+                            <a class="btn btn-primary w-100 w-md-auto"  data-bs-target="#templateModal" data-bs-toggle="modal" href="">
                                 <i data-feather="plus"></i>
                                 Create Template
                             </a>
@@ -162,7 +162,56 @@
         'title' => 'Delete Templates',
         'confirmText' => 'Are you sure you want to delete this items?',
         ])
+        <div class="modal new-user-modal fade" id="templateModal">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="add-new-user modal-content pt-0 px-1">
 
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
+                    <div class="modal-header mb-1 border-0 p-0">
+                        <h5 class="modal-title fs-4">Select Product to add template</h5>
+
+                    </div>
+                    <form action="{{ route("check.product.type") }}" method="post">
+                        @csrf
+                        <div class="modal-body flex-grow-1 d-flex flex-column gap-2">
+                            <div class="form-check option-box rounded border py-1 px-3 d-flex align-items-center">
+                                <input
+                                    class="form-check-input me-2"
+                                    type="radio"
+                                    name="product_type"
+                                    id="codeTshirt"
+                                    value="T-shirt"
+                                />
+                                <label class="form-check-label mb-0 flex-grow-1" for="codeTshirt">T-shirt</label>
+                            </div>
+                            <div class="form-check option-box rounded border py-1 px-3 d-flex align-items-center">
+                                <input
+                                    class="form-check-input me-2"
+                                    type="radio"
+                                    name="product_type"
+                                    id="codeOther"
+                                    value="other"
+                                />
+                                <label class="form-check-label mb-0 flex-grow-1" for="codeOther">Other</label>
+                            </div>
+                        </div>
+                        <div class="modal-footer border-top-0 d-flex justify-content-end gap-2">
+                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="submit"
+                                    class="btn btn-primary ">
+                                Next
+                            </button>
+                    </form>
+
+
+
+
+
+                </div>
+
+            </div>
+        </div>
+        </div>
         <!-- list and filter end -->
     </section>
     <!-- users list ends -->
