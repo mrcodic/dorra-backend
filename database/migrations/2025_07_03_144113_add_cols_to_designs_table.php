9da5cb9ba33524cc6fce4694dd0a8e187a480f4a
yomna <yomnaali463@gmail.com>
2025-07-07 13:46:08 +0300

    ADD COLS TO DESIGNS TABLE

--- database/migrations/2025_07_03_144113_add_cols_to_designs_table.php
+++ database/migrations/2025_07_03_144113_add_cols_to_designs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,10 @@ return new class extends Migration
             $table->after('id', function ($table) {
                 $table->string('name')->nullable();;
                 $table->string('description')->nullable();
+                $table->integer("height")->nullable();
+                $table->integer("width")->nullable();
+                $table->tinyInteger("unit")->nullable();
+                $table->foreignIdFor(Product::class)->nullable()->constrained()->cascadeOnDelete();
             });
         });
     }
@@ -27,6 +32,11 @@ return new class extends Migration
         Schema::table('designs', function (Blueprint $table) {
             $table->dropColumn('name');
             $table->dropColumn('description');
+            $table->dropColumn('height');
+            $table->dropColumn('width');
+            $table->dropColumn('unit');
+            $table->dropForeign('product_id');
+            $table->dropColumn('product_id');
         });
     }
 };
