289a815320130307f536368bd36acee96290e646
yomna <yomnaali463@gmail.com>
2025-07-14 12:38:24 +0300

    Update PaymobStrategy.php

--- app/Services/Payment/PaymobStrategy.php
+++ app/Services/Payment/PaymobStrategy.php
@@ -50,12 +50,13 @@ use Illuminate\Support\Facades\Log;
         $dto = PaymobIntentionData::fromArray(
             data: [
                 ...$payload,
-                'redirection_url' => $this->config['notification_url'] ?? '',
+                'redirection_url' => $this->config['redirection_url'] ?? '',
                 'notification_url' => $this->config['notification_url'] ?? '',
             ],
             integrationId: (int)$integrationIds[$paymentMethod] ?? throw new \Exception("Invalid payment method"),
             currency: $this->config['currency'] ?? 'EGP'
         );
+        dd($dto->toArray());
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $this->config['secret_key'],
             'Content-Type' => 'application/json',
