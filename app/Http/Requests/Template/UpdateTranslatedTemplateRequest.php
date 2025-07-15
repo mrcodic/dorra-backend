e38b61eb4c87ff4b87b78f257b2b1cda70fe585f
yomna <yomnaali463@gmail.com>
2025-07-15 14:58:55 +0300

    Update UpdateTranslatedTemplateRequest.php

--- app/Http/Requests/Template/UpdateTranslatedTemplateRequest.php
+++ app/Http/Requests/Template/UpdateTranslatedTemplateRequest.php
@@ -50,7 +50,7 @@ class UpdateTranslatedTemplateRequest extends BaseRequest
             ],
             'type' => ['sometimes','in:'.TypeEnum::getValuesAsString()],
             'status' => ["required","integer","in:".StatusEnum::getValuesAsString()],
-            'product_id' => ['required', 'exists:products,id'],
+            'product_id' => ['sometimes', 'exists:products,id'],
             'design_data' => ['sometimes', 'json'],
             'base64_preview_image' => ['sometimes', 'string'],
             'specifications' => ['sometimes', 'array'],
