25172fa802d4f5fae5d6fdd40dc5d81a9dc587ec
yomna <yomnaali463@gmail.com>
2025-07-17 12:20:38 +0300

    Update OrderData.php

--- app/DTOs/Order/OrderData.php
+++ app/DTOs/Order/OrderData.php
@@ -10,16 +10,15 @@ class OrderData
 {
     public static function fromCart($subTotal, $discountCode): array
     {
-        $discountAmount = getDiscountAmount($discountCode ?? 0, $subTotal);
         return [
             'user_id' => Auth::guard('sanctum')?->id(),
             'guest_id' => request()->hasCookie('cookie_id') ? Guest::whereCookieValue(request()->cookie('cookie_id'))->first()?->id : null,
             'payment_method_id' => request('payment_method_id'),
             'subtotal' => $subTotal,
-            'discount_amount' => $discountAmount,
+            'discount_amount' => getDiscountAmount($discountCode ?? 0, $subTotal),
             'delivery_amount' => setting('delivery') ?? 30,
             'tax_amount' => setting('tax'),
-            'total_price' => $subTotal - $discountAmount,
+            'total_price' => getTotalPrice($discountCode ?? 0, $subTotal),
             'status' => StatusEnum::PENDING,
         ];
     }
