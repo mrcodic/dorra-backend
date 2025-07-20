eca5b431deae5e9c218bd8b1edb4d1d78eea9dbb
yomna <yomnaali463@gmail.com>
2025-07-20 10:39:54 +0300

    Update UpdateProductRequest.php

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -58,6 +58,7 @@ class UpdateProductRequest extends BaseRequest
             }],
             'tags' => ['nullable', 'array'],
             'has_custom_prices' => ['required', 'boolean'],
+            'has_mockup' => ['required', 'boolean'],
             'base_price' => [
                 'required_if:has_custom_prices,false',
                 'prohibited_if:has_custom_prices,true',
