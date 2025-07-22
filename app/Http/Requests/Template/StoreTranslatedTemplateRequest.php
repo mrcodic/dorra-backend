1af315a3b09322ada605cd297dc014aaf38b2b38
yomna <yomnaali463@gmail.com>
2025-07-22 11:00:37 +0300

    Update StoreTranslatedTemplateRequest.php

--- app/Http/Requests/Template/StoreTranslatedTemplateRequest.php
+++ app/Http/Requests/Template/StoreTranslatedTemplateRequest.php
@@ -51,6 +51,8 @@ class StoreTranslatedTemplateRequest extends BaseRequest
             'product_ids.*' => ['integer', 'exists:products,id'],
             'tags' => ['sometimes', 'array'],
             'tags.*' => ['integer', 'exists:tags,id'],
+            'types' => ['required', 'array'],
+            'types.*' => ['integer', 'exists:types,id'],
             'design_data' => ['sometimes', 'json'],
             'base64_preview_image' => ['sometimes', 'string'],
             'specifications' => ['sometimes', 'array'],
