c47d15cc51fef6c33f6fc26cf7ab966d8b3de09f
yomna ali <yomnaali463@gmail.com>
2025-07-23 10:31:18 +0300

    has_mockup

--- app/Http/Controllers/Dashboard/TemplateController.php
+++ app/Http/Controllers/Dashboard/TemplateController.php
@@ -94,7 +94,7 @@ class TemplateController extends DashboardController
             "redirect_url" =>
                 config('services.editor_url') .
                 'templates/' . $template->id . "?has_mockup=" .
-                ($template->products->pluck('has_mockup')->contains(true) ? 'true' : 'false')
+                (\App\Models\Product::where('template_id', $template->id)->where('has_mockup', true)->exists())
         ]);
     }
 
