67d02ce48da432213f9b4407145733ef46548077
yomna <yomnaali463@gmail.com>
2025-07-24 10:52:59 +0300

    UNIT CM

--- app/Http/Resources/DimensionResource.php
+++ app/Http/Resources/DimensionResource.php
@@ -18,8 +18,6 @@ class DimensionResource extends JsonResource
             'id' => $this->id,
             'width' => $this->when(isset($this->width_pixel), $this->width_pixel),
             'height' => $this->when(isset($this->height_pixel), $this->height_pixel),
-            'original_width' => $this->when(isset($this->width), $this->width),
-            'original_height' => $this->when(isset($this->height), $this->height),
             'dpi' => 300,
             'unit' => $this->when(isset($this->unit), [
                 'value' => $this->unit?->value,
--- app/Models/Dimension.php
+++ app/Models/Dimension.php
@@ -20,18 +20,18 @@ class Dimension extends Model
     {
         return $this->belongsToMany(Product::class)->withTimestamps();
     }
-    public function getWidthPixelAttribute()
+    public function getWidthCmAttribute()
     {
-        $value = $this->unit === UnitEnum::CM
-            ? round($this->width / 2.54, 2)
+        $value = $this->unit === UnitEnum::PIXEL
+            ? round($this->width * 2.54, 2)
             : $this->width;
         return fmod($value, 1) == 0.0 ? (int)$value : $value;
     }
 
-    public function getHeightPixelAttribute()
+    public function getHeightCmAttribute()
     {
-        $value = $this->unit === UnitEnum::CM
-            ? round($this->height / 2.54, 2)
+        $value = $this->unit === UnitEnum::PIXEL
+            ? round($this->height * 2.54, 2)
             : $this->height;
 
         return fmod($value, 1) == 0.0 ? (int)$value : $value;
