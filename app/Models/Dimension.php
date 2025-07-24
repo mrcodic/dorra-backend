f3b1224101e31d39e33155460856fd96faa5ea06
yomna <yomnaali463@gmail.com>
2025-07-24 11:40:05 +0300

    Update Dimension.php

--- app/Models/Dimension.php
+++ app/Models/Dimension.php
@@ -23,7 +23,7 @@ class Dimension extends Model
     public function getWidthCmAttribute()
     {
         $value = $this->unit === UnitEnum::PIXEL
-            ? round($this->width * 2.54, 2)
+            ? round($this->width * 2.54 / 300, 2)
             : $this->width;
         return fmod($value, 1) == 0.0 ? (int)$value : $value;
     }
@@ -31,7 +31,7 @@ class Dimension extends Model
     public function getHeightCmAttribute()
     {
         $value = $this->unit === UnitEnum::PIXEL
-            ? round($this->height * 2.54, 2)
+            ? round($this->height * 2.54 / 300, 2)
             : $this->height;
 
         return fmod($value, 1) == 0.0 ? (int)$value : $value;
