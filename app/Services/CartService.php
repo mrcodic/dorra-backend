35ba881c98d39172c15ac1ea85fc905f2e59379d
yomna <yomnaali463@gmail.com>
2025-07-27 12:17:35 +0300

    Update CartService.php

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -102,7 +102,9 @@ class CartService extends BaseService
         return $this->repository->query()
             ->when($userId, fn($q) => $q->where('user_id', $userId))
             ->when(!$userId && $guestId, fn($q) => $q->where('guest_id', $guestId))
-            ->with(['items.product'])
+            ->with(['items.product','items.itemable' => function ($query) {
+                $query->select(['id','name']);
+            }])
             ->first();
     }
 
