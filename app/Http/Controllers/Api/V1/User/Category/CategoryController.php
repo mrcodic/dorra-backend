716fc1952e6d7d5710bb2497e602242e55aca807
yomna <yomnaali463@gmail.com>
2025-08-03 11:22:29 +0300

    Update CategoryController.php

--- app/Http/Controllers/Api/V1/User/Category/CategoryController.php
+++ app/Http/Controllers/Api/V1/User/Category/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1\User\Category;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Services\CategoryService;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Response;
 
 
@@ -16,7 +17,11 @@ class CategoryController extends Controller
 
     public function index()
     {
-        return Response::api(data: CategoryResource::collection($this->categoryService->getAll(relations: ['media'])));
+        $categories = $this->categoryService->getAll(relations: ['media'],paginate: request('paginate',false),perPage: request('per_page',8));
+        $categoryResourceCollection = $categories instanceof LengthAwarePaginator ?
+            CategoryResource::collection($categories)->response()->getData()
+            : CategoryResource::collection($categories);
+        return Response::api(data: $categoryResourceCollection);
     }
 
     public function show($id)
