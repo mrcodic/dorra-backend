f6f78a3933520d8107afe74dd37308ad0865d7a1
yomna <yomnaali463@gmail.com>
2025-08-18 15:08:06 +0300

    Update StoreFolderRequest.php

--- app/Http/Requests/Folder/StoreFolderRequest.php
+++ app/Http/Requests/Folder/StoreFolderRequest.php
@@ -24,16 +24,16 @@ class StoreFolderRequest extends BaseRequest
                 'nullable',
                 'string',
                 'exists:designs,id',
-                function ($attribute, $value, $fail) {
-                    $design = Design::with('users')->find($value);
-
-                    if (
-                        !$design ||
-                        !$design->users->contains('id', auth('sanctum')->id())
-                    ) {
-                        $fail("The selected design does not belong to you or you are not a member of this design.");
-                    }
-                },
+//                function ($attribute, $value, $fail) {
+//                    $design = Design::with('users')->find($value);
+//
+////                    if (
+////                        !$design ||
+////                        !$design->users->contains('id', auth('sanctum')->id())
+////                    ) {
+////                        $fail("The selected design does not belong to you or you are not a member of this design.");
+////                    }
+//                },
             ],
         ];
 
