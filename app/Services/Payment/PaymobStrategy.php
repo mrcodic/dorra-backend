3d4469d80a9be9848269edd80f5e48894e42adc5
yomna <yomnaali463@gmail.com>
2025-08-19 16:02:13 +0300

    Update PaymobStrategy.php

--- app/Services/Payment/PaymobStrategy.php
+++ app/Services/Payment/PaymobStrategy.php
@@ -67,7 +67,7 @@ use Illuminate\Support\Facades\Log;
             'response' => $result,
             'vzx' => $response,
         ]);
-        dd($result, $dto->toArray());
+     
         if ($response->failed() || empty($result['client_secret']) || empty($result['id'])) {
             Log::error('Failed to create payment intention', [
                 'response' => $result,
