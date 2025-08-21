f921061597aca76500c12a731cbc26c0ae9b519a
yomna <yomnaali463@gmail.com>
2025-08-21 16:47:10 +0300

    Update ProductPrice.php

--- app/Models/ProductPrice.php
+++ app/Models/ProductPrice.php
@@ -15,13 +15,14 @@ class ProductPrice extends Model
     protected static function booted()
     {
         static::updated(function (ProductPrice $productPrice) {
-            if ($productPrice->wasChanged('price'))
+            if ($productPrice->wasChanged('price') || $productPrice->wasChanged('quantity'))
             {
                 $product = $productPrice->product;
                 CartItem::where('product_id', $product->id)->get()
                     ->each(function ($item) use ($product) {
                         $item->update([
                             'product_price' => $product->price,
+                            'quantity' => $product->quantity,
                             'sub_total'     => $product->price + $item->specs_price - $item->cart->discount_amount,
                         ]);
                     });
