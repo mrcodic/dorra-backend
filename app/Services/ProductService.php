67066af83dc83d8f331864a52fb91f236614458f
yomna <yomnaali463@gmail.com>
2025-08-24 11:22:22 +0300

    Update ProductService.php

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -224,6 +224,7 @@ class ProductService extends BaseService
             $this->handleTransaction(function () use ($product, $validatedData) {
                 $product->update(['base_price' => null]);
                 if ($product->isDirty('has_custom_prices')) {
+                    dd("fdssd",$product->prices()->first()->quantity);
                     CartItem::where('product_id', $product->id)->get()
                         ->each(function ($item) use ($product) {
                             $item->update([
