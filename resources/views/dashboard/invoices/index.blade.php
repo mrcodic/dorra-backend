fb6404ecdf74c55eb5572de1ae5c0043b518e467
yomna <yomnaali463@gmail.com>
2025-08-25 10:33:44 +0300

    Update index.blade.php

--- resources/views/dashboard/invoices/index.blade.php
+++ resources/views/dashboard/invoices/index.blade.php
@@ -193,11 +193,7 @@
                             Export
                         </a>
                     </div>
-
-                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
-                        data-bs-target="#addAdminModal">
-                        Export
-                    </button>
+                    
                 </div>
 
                 <table class="order-list-table table">
