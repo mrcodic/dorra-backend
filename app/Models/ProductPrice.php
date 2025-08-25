d61d26e02798ac9f038c552a3dc69af8206f5087
yomna <yomnaali463@gmail.com>
2025-08-25 11:42:22 +0300

    Update ProductPrice.php

--- app/Models/ProductPrice.php
+++ app/Models/ProductPrice.php
@@ -25,6 +25,8 @@ class ProductPrice extends Model
 
                         if ((float)$productPriceValue < (float)$item->cart?->discount_amount)
                         {
+                            dd($productPriceValue,$item->cart?->discount_amount);
+
                             $item->cart->update([
                                 'discount_amount' => 0,
                                 'discount_code_id' => null,
