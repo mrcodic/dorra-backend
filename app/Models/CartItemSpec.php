df8ebd73a4bedac0638fd8e9f287e9bce01fdc4d
yomna <yomnaali463@gmail.com>
2025-09-01 12:25:46 +0300

    Update CartItemSpec.php

--- app/Models/CartItemSpec.php
+++ app/Models/CartItemSpec.php
@@ -14,6 +14,29 @@ class CartItemSpec extends Model
         'spec_option_id',
         'cart_item_id',
     ];
+    protected static function booted()
+    {
+        static::created(fn ($item) => $item->recalculateCartItem());
+        static::deleted(fn ($item) => $item->recalculateCartItem());
+        static::updated(fn ($item) => $item->recalculateCartItem());
+    }
+
+    public function recalculateCartItem(): void
+    {
+        $cartItem = $this->cartItem;
+        if ($cartItem) {
+            $newSpecsPrice = $cartItem->specs
+                ->map(fn ($spec) => $spec->productSpecificationOption?->price ?? 0)
+                ->sum();
+
+            $cartItem->update([
+                'specs_price' => $newSpecsPrice,
+                'sub_total'   => $cartItem->product->has_custom_prices
+                    ? $cartItem->product_price + $newSpecsPrice
+                    : ($cartItem->product_price * $cartItem->quantity) + $newSpecsPrice,
+            ]);
+        }
+    }
 
     public function productSpecification(): BelongsTo
     {
