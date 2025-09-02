9244e652f0956c20964a1190ba3c8a1e3dc50a8e
yomna <yomnaali463@gmail.com>
2025-09-02 14:26:09 +0300

    Update CategoryService.php

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -46,7 +46,7 @@ class CategoryService extends BaseService
     public function updateResource($validatedData, $id, $relationsToLoad = [])
     {
         $model = $this->repository->update($validatedData, $id);
-        if (Arr::has($validatedData, 'image_id')) {
+        if (Arr::has($validatedData, 'image_id')&& !is_null($validatedData['image_id'])) {
             $model->clearMediaCollection('categories');
             Media::where('id', $validatedData['image_id'])
                 ->update([
