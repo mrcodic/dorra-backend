4abe3d94329fec0c56116dffffa5b7b6b804da18
yomna <yomnaali463@gmail.com>
2025-09-03 15:23:42 +0300

    Update Design.php

--- app/Models/Design.php
+++ app/Models/Design.php
@@ -17,9 +17,8 @@ use Spatie\Translatable\HasTranslations;
 #[ObservedBy(DesignObserver::class)]
 class Design extends Model implements HasMedia
 {
-    use HasUuids, InteractsWithMedia, softDeletes;
-//    public $translatable = ['name', 'description'];
-
+    use HasUuids, InteractsWithMedia, HasTranslations, softDeletes;
+    public $translatable = ['name', 'description'];
 
     protected $fillable = [
         'cookie_id',
