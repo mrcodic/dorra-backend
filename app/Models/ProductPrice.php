1f6e19af2d3269edcfba4b5bb5e793ae194ecf91
yomna <yomnaali463@gmail.com>
2025-09-09 16:00:50 +0300

    Update ProductPrice.php

--- app/Models/ProductPrice.php
+++ app/Models/ProductPrice.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ProductPrice extends Model
 {
     protected $fillable = [
-        'product_id',
         'quantity',
         'price',
     ];
