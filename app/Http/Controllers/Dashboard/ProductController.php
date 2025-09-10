e135b2a63b5eac8a13ae614bdc2040a3900be1c2
yomna <yomnaali463@gmail.com>
2025-09-10 10:58:04 +0300

    Update ProductController.php

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -35,7 +35,7 @@ class ProductController extends DashboardController
 
         $this->assoiciatedData = [
             'shared' => [
-                'categories' => $this->categoryRepository->query()->whereNull('parent_id')->whereIsHasCategory(0)->get(['id', 'name']),
+                'categories' => $this->categoryRepository->query()->whereNull('parent_id')->whereIsHasCategory(1)->get(['id', 'name']),
                 'tags' => $this->tagRepository->all(columns: ['id', 'name']),
                 'dimensions' =>  $this->dimensionRepository->query()->whereIsCustom(false)->get(['id', 'name']),
             ],
