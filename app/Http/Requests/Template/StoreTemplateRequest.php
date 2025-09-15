1680f5670c7f2fafd552f5cf0ad481643aa5b899
yomna <yomnaali463@gmail.com>
2025-09-15 16:53:23 +0300

    debug

--- app/Http/Requests/Template/StoreTemplateRequest.php
+++ app/Http/Requests/Template/StoreTemplateRequest.php
@@ -39,7 +39,7 @@ class StoreTemplateRequest extends BaseRequest
             'specifications' => ['sometimes', 'array'],
             'specifications.*' => ['sometimes', 'integer', 'exists:product_specifications,id'],
             'source_design_svg' => ['nullable', 'file'],
-            'product_type' => ['sometimes', 'in:T-shirt,other'],
+
         ];
     }
 
--- app/Http/Requests/Template/StoreTranslatedTemplateRequest.php
+++ app/Http/Requests/Template/StoreTranslatedTemplateRequest.php
@@ -27,38 +27,27 @@ class StoreTranslatedTemplateRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            'name.en' => [
-                'required',
-                'string',
-                'max:255',
-            ],
-            'name.ar' => [
-                'required',
-                'string',
-                'max:255',
-            ],
-            'description.en' => [
-                'nullable',
-                'string',
-            ],
-            'description.ar' => [
-                'nullable',
-                'string',
-            ],
-            'type' => ['sometimes','in:'.TypeEnum::getValuesAsString()],
-            'unit' => ["nullable","integer","in:".UnitEnum::getValuesAsString()],
-            'product_ids' => ['required', 'array'],
+            'name.en' => ['required', 'string', 'max:255'],
+            'name.ar' => ['required', 'string', 'max:255'],
+            'description.en' => ['nullable', 'string'],
+            'description.ar' => ['nullable', 'string'],
+            'type' => ['sometimes', 'in:' . TypeEnum::getValuesAsString()],
+            'product_ids' => ['required_without:category_ids','required_with:product_with_category', 'array'],
             'product_ids.*' => ['integer', 'exists:products,id'],
+            'category_ids' => ['required_without:product_ids', 'array'],
+            'category_ids.*' => ['integer', 'exists:categories,id'],
             'tags' => ['sometimes', 'array'],
             'tags.*' => ['integer', 'exists:tags,id'],
             'types' => ['required', 'array'],
             'types.*' => ['integer', 'exists:types,id'],
+            'template_image_id' => ['required','exists:media,id'],
             'design_data' => ['sometimes', 'json'],
             'design_back_data' => ['sometimes', 'json'],
             'base64_preview_image' => ['sometimes', 'string'],
+            'back_base64_preview_image' => ['sometimes', 'string'],
             'specifications' => ['sometimes', 'array'],
             'specifications.*' => ['sometimes', 'integer', 'exists:product_specifications,id'],
-            'source_design_svg' => ['nullable', 'file', 'mimetypes:image/svg+xml', 'max:2048'],
+            'source_design_svg' => ['nullable', 'file'],
         ];
 
     }
