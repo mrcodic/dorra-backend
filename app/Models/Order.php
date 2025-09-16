f013becea0150fbcd6b8c1869b63fe27706f46e2
yomna <yomnaali463@gmail.com>
2025-09-16 15:09:24 +0300

    order cards

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use App\Enums\Order\StatusEnum;
 use App\Observers\OrderObserver;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, BelongsToMany, HasMany, HasOne};
@@ -46,6 +47,10 @@ class Order extends Model
         });
     }
 
+    public function scopeStatus(Builder $query, $status): Builder
+    {
+        return $query->whereStatus($status);
+}
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
