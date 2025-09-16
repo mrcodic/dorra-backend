ef33c5ea2a447a930b54ff1f45f0c5890df0cf68
yomna <yomnaali463@gmail.com>
2025-09-16 14:18:10 +0300

    translation

--- lang/ar/orders.php
+++ lang/ar/orders.php
@@ -1,13 +1,13 @@
 <?php
 return [
     'status' => [
-        'pending'   => 'قيد الانتظار',
+        'pending' => 'قيد الانتظار',
         'confirmed' => 'تم التأكيد',
-        'prepared'  => 'تم التجهيز',
-        'shipped'   => 'تم الشحن',
+        'prepared' => 'تم التجهيز',
+        'shipped' => 'تم الشحن',
         'delivered' => 'تم التسليم',
-        'refunded'  => 'تم الاسترجاع',
-        'payment_failed_message' => 'حدث خطأ ما',
-        'payment_failed_error'   => 'فشل في عملية الدفع، الرجاء المحاولة مرة أخرى لاحقًا.',
-    ],
+        'refunded' => 'تم الاسترجاع',
+
+    ], 'payment_failed_message' => 'حدث خطأ ما',
+    'payment_failed_error' => 'فشل في عملية الدفع، الرجاء المحاولة مرة أخرى لاحقًا.',
 ];
