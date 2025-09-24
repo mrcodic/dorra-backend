17b72b0cb8fbc24ea0e8394f437429bbeb7954c0
yomna <yomnaali463@gmail.com>
2025-09-24 17:00:14 +0300

    Update MainController.php

--- app/Http/Controllers/Shared/General/MainController.php
+++ app/Http/Controllers/Shared/General/MainController.php
@@ -246,7 +246,9 @@ class MainController extends Controller
             ->when($rates, function ($q) use ($rates) {
                 $q->where(function ($qq) use ($rates) {
                     $qq->whereHas('products', fn($p) => $p->withReviewRating($rates))
-                        ->orWhereHas('reviews', fn($r) => $r->whereIn('rating', $rates));
+                        ->orWhere(function ($qq) use ($rates) {
+                            $qq->withReviewRating($rates);
+                        });
                 });
             })
             ->get();
