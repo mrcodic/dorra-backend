f47a218d3efe52877d6eed5ebc4e5a262aa158c9
yomna <yomnaali463@gmail.com>
2025-09-29 14:46:29 +0300

    Update JobTicketObserver.php

---
+++ app/Observers/JobTicketObserver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Observers;
+
+use App\Enums\JobTicket\StatusEnum;
+use App\Models\JobTicket;
+use App\Models\Station;
+
+class JobTicketObserver
+{
+
+    public function updating(JobTicket $jobTicket): void
+    {
+        if (! $jobTicket->isDirty('station_id')) {
+            return;
+        }
+
+        $stationCode = optional($jobTicket->station)->code
+            ?? Station::whereKey($jobTicket->station_id)->value('code');
+
+        $jobTicket->status = $this->statusForStation($stationCode);
+    }
+
+
+    private function statusForStation(?string $code): StatusEnum
+    {
+        $code = $code ? strtolower($code) : null;
+
+        return match ($code) {
+            'print'    => StatusEnum::PRINT_QUEUE,
+            'finish'   => StatusEnum::FINISH_QUEUE,
+            'qc'       => StatusEnum::QC_QUEUE,
+            'pack'     => StatusEnum::PACK_QUEUE,
+            default    => StatusEnum::PREPRESS_QUEUE,
+        };
+    }
+}
