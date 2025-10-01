54ba8e25b74de21fb2d41f41ec42ac5bb8460a70
yomna <yomnaali463@gmail.com>
2025-10-01 14:40:17 +0300

    Update JobTicketService.php

--- app/Services/JobTicketService.php
+++ app/Services/JobTicketService.php
@@ -122,6 +122,7 @@ class JobTicketService extends BaseService
                 })(),
 
                 $nextStation && $firstStatusOfNext => (function () use ($ticket, $nextStation, $firstStatusOfNext) {
+                    dd($firstStatusOfNext , $nextStation);
                     $this->eventRepository->create([
                         'job_ticket_id'      => $ticket->id,
                         'station_id'         => $nextStation->id,
