5f32b3f3a983e1ef4f790774fed9ea9266b0b65d
yomna <yomnaali463@gmail.com>
2025-10-01 15:35:23 +0300

    Update JobTicketService.php

--- app/Services/JobTicketService.php
+++ app/Services/JobTicketService.php
@@ -105,7 +105,7 @@ class JobTicketService extends BaseService
             $firstStatusOfNext = $nextStation
                 ? $nextStation->statuses()->orderBy('sequence')->first()
                 : null;
-            dd($nextStatusInSame && !($nextStation && $firstStatusOfNext));
+            dd($nextStatusInSame ,!($nextStation && $firstStatusOfNext));
 
           match (true) {
                 (bool) $nextStatusInSame && !($nextStation && $firstStatusOfNext) => (function () use ($ticket, $station, $nextStatusInSame) {
