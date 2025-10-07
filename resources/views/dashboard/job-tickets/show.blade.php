01959dee41f9e548baab9a929a8628b416fb67ab
yomna <yomnaali463@gmail.com>
2025-10-07 11:49:08 +0300

    Update show.blade.php

--- resources/views/dashboard/job-tickets/show.blade.php
+++ resources/views/dashboard/job-tickets/show.blade.php
@@ -153,7 +153,7 @@
                 </div>
                 {{-- Codes card --}}
                 <div class="row g-1 text-center mt-2">
-                    @if($model->status !== \App\Enums\JobTicket\StatusEnum::PENDING)
+{{--                    @if($model->status !== \App\Enums\JobTicket\StatusEnum::PENDING)--}}
                     <div class="col-12">
                         {{-- Code128 --}}
                         <img src="{{ $model->qr_png_url }}" alt="Code128"
@@ -167,7 +167,7 @@
                             class="img-fluid border rounded p-2 w-100">
                     </div>
                 </div>
-                @endif
+{{--                @endif--}}
                 {{-- Specifications --}}
                 @php
                 $specsRaw = $model->specs ?? []; // or the JSON you have
