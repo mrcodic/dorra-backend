5760e06871e98eb7dba753816cf780bc7a92faaa
yomna <yomnaali463@gmail.com>
2025-10-07 17:08:43 +0300

    quantity

--- app/Services/JobTicketService.php
+++ app/Services/JobTicketService.php
@@ -71,6 +71,7 @@ class JobTicketService extends BaseService
             ->addColumn('current_station', fn($job) => $job->station?->name ?? '-')
             ->addColumn('order_number', fn($job) => $job->orderItem->order->order_number ?? '-')
             ->addColumn('order_item_name', fn($job) => $job->orderItem->orderable?->name ?? '-')
+            ->addColumn('order_item_quantity', fn($job) => $job->orderItem?->quantity ?? '-')
             ->addColumn('order_item_id', fn($job) => $job->orderItem?->id ?? '-')
             ->addColumn('order_item_image', fn($job) => $job->orderItem->itemable->getFrontImageUrl())
             ->make(true);
--- resources/views/dashboard/job-tickets/index.blade.php
+++ resources/views/dashboard/job-tickets/index.blade.php
@@ -348,14 +348,15 @@
                                 <input type="checkbox" id="select-all-checkbox" class="form-check-input">
                             </th>
                             <th>Image</th>
-                            <th>Job Number</th>
+                            <th>Item Id</th>
+                            <th>Item Name</th>
+                            <th>Item Quantity</th>
+                            <th>Order Number</th>
                             <th>Priority</th>
                             <th>Current Station</th>
                             <th>Status</th>
                             <th>Due Date</th>
-                            <th>Order Number</th>
-                            <th>Item Name</th>
-                            <th>Item Id</th>
+
 
                             <th>Actions</th>
                         </tr>
