fca461447658d041973a3ee02f6b834b81ebc951
yomna <yomnaali463@gmail.com>
2025-10-08 15:44:20 +0300

    Update ProductService.php

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -295,22 +295,22 @@ class ProductService extends BaseService
                     })->toArray();
 
 
-                    $productSpecification->options()->whereNotIn('id', $submittedOptionIds)->each(function ($option) {
-                        $option->clearMediaCollection();
-                        $option->delete();
-                    });
+//                    $productSpecification->options()->whereNotIn('id', $submittedOptionIds)->each(function ($option) {
+//                        $option->clearMediaCollection();
+//                        $option->delete();
+//                    });
 
                     return $productSpecification->id;
                 })->toArray();
 
 
-                $product->specifications()->whereNotIn('id', $submittedSpecIds)->each(function ($spec) {
-                    $spec->options->each(function ($option) {
-                        $option->clearMediaCollection();
-                        $option->delete();
-                    });
-                    $spec->delete();
-                });
+//                $product->specifications()->whereNotIn('id', $submittedSpecIds)->each(function ($spec) {
+//                    $spec->options->each(function ($option) {
+//                        $option->clearMediaCollection();
+//                        $option->delete();
+//                    });
+//                    $spec->delete();
+//                });
             } else {
 
                 $product->specifications->each(function ($spec) {
