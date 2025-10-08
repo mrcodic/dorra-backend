0919091397a9ffa6a51552615c45e19021a7015b
yomna <yomnaali463@gmail.com>
2025-10-08 09:40:14 +0300

    offers

--- resources/views/modals/offers/show-offer.blade.php
+++ resources/views/modals/offers/show-offer.blade.php
@@ -1,7 +1,7 @@
 <div class="modal modal-slide-in new-user-modal fade" id="showOfferModal">
     <div class="modal-dialog">
         <div class="add-new-user modal-content pt-0">
-            <form id="addDiscountForm" method="post" enctype="multipart/form-data" action="{{ route('discount-codes.store') }}">
+            <form id="addDiscountForm" method="post" enctype="multipart/form-data" action="">
                 @csrf
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
                 <div class="modal-header mb-1">
