9f0be2c0b3f947534d50ef8bfe4d0f6e8ed2cb78
yomna <yomnaali463@gmail.com>
2025-10-16 15:10:25 +0300

    Update app-station-status-list.js

--- public/js/scripts/pages/app-station-status-list.js
+++ public/js/scripts/pages/app-station-status-list.js
@@ -146,8 +146,8 @@ $(document).on('click', '.edit-details', function (e) {
     editSetMode(mode);
 
     // IDs from the row
-    const resourceableId = String($b.data('resourceableId') || '');       // saved category id (when with) or product id (when without)
-    const parentId       = String($b.data('resourceableParentId') || ''); // saved product id (when with)
+    // const resourceableId = String($b.data('resourceableId') || '');       // saved category id (when with) or product id (when without)
+    // const parentId       = String($b.data('resourceableParentId') || ''); // saved product id (when with)
     const resourceLabel  = $b.data('resource') || `#${resourceableId}`;
 
     if (mode === 'with') {
