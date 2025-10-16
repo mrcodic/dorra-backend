b03ab1c9019a9c882fe3ce6a83c327d581be89ef
yomna <yomnaali463@gmail.com>
2025-10-16 09:56:59 +0300

    Update index.blade.php

--- resources/views/dashboard/station-statuses/index.blade.php
+++ resources/views/dashboard/station-statuses/index.blade.php
@@ -199,14 +199,8 @@
                     <div id="bulk-delete-container" class="my-2 bulk-delete-container" style="display: none;">
                         <div class="delete-container d-flex flex-wrap align-items-center justify-content-center justify-content-md-between"
                              style="z-index: 10;">
-                            <p id="selected-count-text">0 Custom Statuses are selected</p>
-                            <button type="submit" id="delete-selected-btn" data-bs-toggle="modal"
-                                    data-bs-target="#deleteInventoriesModal"
-                                    class="btn btn-outline-danger d-flex justify-content-center align-items-center gap-1 delete-selected-btns">
-                                <i data-feather="trash-2"></i> Delete Selected
-                            </button>
-                            <form style="display: none;" id="bulk-delete-form" method="POST"
-                                  action="{{ route('station-statuses.bulk-delete') }}">
+                            <p id="selected-count-text">0 offers are selected</p>
+                            <form id="bulk-delete-form" method="POST" action="{{ route('station-statuses.bulk-delete') }}">
                                 @csrf
                                 <button type="submit" id="delete-selected-btn"
                                         class="btn btn-outline-danger d-flex justify-content-center align-items-center gap-1 delete-selected-btns">
@@ -215,8 +209,6 @@
                             </form>
                         </div>
                     </div>
-                </div>
-
 
                 @include('modals.delete', [
                 'id' => 'deleteStatusModal',
