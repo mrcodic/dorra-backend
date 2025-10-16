0739ca6f4507349ec3c7a3595650e74a58cfdcf2
yomna <yomnaali463@gmail.com>
2025-10-16 15:27:52 +0300

    Update app-station-status-list.js

--- public/js/scripts/pages/app-station-status-list.js
+++ public/js/scripts/pages/app-station-status-list.js
@@ -143,7 +143,7 @@ $(document).on('click', '.edit-details', function (e) {
     editSetMode(mode);
 
     const resourceableId = $b.data('resourceableId') || ''; // category id (in "with"
-    console.log(resourceableId)
+    console.log(resourceableId,resourceLabel)
     const parentId       = $b.data('parentId') || '';       // product id (in "with")
     const resourceLabel  = $b.data('resourceLabel') || `#${resourceableId}`;
 
