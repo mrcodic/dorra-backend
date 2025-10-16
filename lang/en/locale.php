81d91a3f46b2a9876a0ed52d83aa3d089ac35096
yomna <yomnaali463@gmail.com>
2025-10-16 16:21:12 +0300

    Custom Statuses

--- lang/en/locale.php
+++ lang/en/locale.php
@@ -6,6 +6,7 @@ return [
     "Dashboards" => "Dashboards",
     "Jobs" => "Jobs",
     "Print command" => "Print Command",
+    "Custom Statuses" => "Custom Statuses",
     "Inventory" => "Inventory",
     "Board" => "Board",
     "Website" => "Website",
