158a71ac55a6fa92782161b095bce270866125e7
yomna <yomnaali463@gmail.com>
2025-10-21 14:39:13 +0300

    Update edit.blade.php

--- resources/views/dashboard/roles/edit.blade.php
+++ resources/views/dashboard/roles/edit.blade.php
@@ -31,13 +31,22 @@
 
 
             <!-- New Role Description field -->
-            <div class="col-12 mt-1">
-                <label class="form-label" for="modalRoleDescription">Role Description</label>
-                <textarea id="modalRoleDescription" name="modalRoleDescription" class="form-control" rows="3"
-                          placeholder="Enter role description"
-                          data-msg="Please enter a description for the role">{{ $model->description }}</textarea>
+            <div class="col-md-6 mt-1">
+                <label class="form-label" for="modalRoleDescription">Role Description (EN)</label>
+                <textarea id="modalRoleDescription" name="description[en]" class="form-control" rows="3"
+                          placeholder="Enter role description in english"
+                          data-msg="Please enter a description for the role">
+                    {{ $model->getTranslation('description','en') }}
+                </textarea>
             </div>
-
+            <div class="col-md-6 mt-1">
+                <label class="form-label" for="modalRoleDescription">Role Description (AR)</label>
+                <textarea id="modalRoleDescription" name="description[ar]" class="form-control" rows="3"
+                          placeholder="Enter role description in arabic"
+                          data-msg="Please enter a description for the role ">
+                     {{ $model->getTranslation('description','ar') }}
+                </textarea>
+            </div>
             <div class="col-12">
                 <h4 class="mt-2 pt-50">Role Permissions</h4>
                 <!-- Permission table -->
