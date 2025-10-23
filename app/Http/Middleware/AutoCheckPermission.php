9dfa9a6e314da344a781ab7e51a12aff333d8159
yomna <yomnaali463@gmail.com>
2025-10-23 15:44:38 +0300

    Update AutoCheckPermission.php

--- app/Http/Middleware/AutoCheckPermission.php
+++ app/Http/Middleware/AutoCheckPermission.php
@@ -18,8 +18,9 @@ class AutoCheckPermission
     {
         $routeName = $request->route()->getName();
         $permission = Permission::query()->whereJsonContains('routes', $routeName)->first();
+        dd($permission,$routeName);
+
         if ($permission) {
-            dd($permission);
             if ($request->user()->cannot($permission->name)) {
                 abort(403);
             }
