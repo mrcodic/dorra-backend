64726e3be8d802f60c389f2cb147d047a35ad127
yomna <yomnaali463@gmail.com>
2025-10-30 10:08:19 +0300

    Update IndustryService.php

--- app/Services/IndustryService.php
+++ app/Services/IndustryService.php
@@ -111,6 +111,7 @@ class IndustryService extends BaseService
     public function getAll($relations = [], bool $paginate = false, $columns = ['*'], $perPage = 10, $counts = [])
     {
         return $this->repository->query()
+            ->with(['children'])
             ->withCount(['templates'])
             ->whereNull('parent_id')
            ->latest()->get();
