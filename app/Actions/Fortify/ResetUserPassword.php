fb0cb06b7cff3bd31443485260371009048266ac
yomna <yomnaali463@gmail.com>
2025-11-13 12:17:55 +0200

    admin reset password

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -16,9 +17,9 @@ class ResetUserPassword implements ResetsUserPasswords
      *
      * @param  array<string, string>  $input
      */
-    public function reset(User $user, array $input): void
+    public function reset(Admin $user, array $input): void
     {
-        dd($input);
+
         Validator::make($input, [
             'password' => $this->passwordRules(),
         ])->validate();
