1a3e3a1bff8182287f4ba8f5626105dc01f9e77c
yomna <yomnaali463@gmail.com>
2025-11-16 09:28:23 +0200

    Update TemplateController.php

--- app/Http/Controllers/Dashboard/TemplateController.php
+++ app/Http/Controllers/Dashboard/TemplateController.php
@@ -49,29 +49,13 @@ class TemplateController extends DashboardController
         $this->resourceTable = 'templates';
         $this->resourceClass = TemplateResource::class;
         $this->assoiciatedData = [
-            'create' => [
+            'shared' => [
                 'products' => $this->productRepository->query()->get(['id', 'name']),
                 'industries' => $this->industryRepository->query()->whereNull('parent_id')->get(['id', 'name']),
                 'sub_industries' => $this->industryRepository->query()->whereNotNull('parent_id')->get(['id', 'name']),
                 'product_with_categories' => $this->categoryRepository->query()->where('is_has_category',1)->has('products')->get(['id', 'name']),
                 'product_without_categories' => $this->categoryRepository->query()->where('is_has_category',0)->get(['id', 'name']),
                 'tags' => $this->tagRepository->query()->get(['id', 'name']),
-                'flags' => $this->flagRepository->query()->get(['id', 'name']),
-
-            ],
-            'index' => [
-                'products' => $this->productRepository->query()->get(['id', 'name']),
-                'tags' => $this->tagRepository->query()->get(['id', 'name']),
-            ],
-            'edit' => [
-                'products' => $this->productRepository->query()->get(['id', 'name']),
-                'product_with_categories' => $this->categoryRepository->query()->where('is_has_category',1)->has('products')->get(['id', 'name']),
-                'product_without_categories' => $this->categoryRepository->query()->where('is_has_category',0)->get(['id', 'name']),
-                'industries' => $this->industryRepository->query()->whereNull('parent_id')->get(['id', 'name']),
-                'sub_industries' => $this->industryRepository->query()->whereNotNull('parent_id')->get(['id', 'name']),
-                'tags' => $this->tagRepository->query()->get(['id', 'name']),
-                'flags' => $this->flagRepository->query()->get(['id', 'name']),
-
             ],
         ];
         $this->methodRelations = [
