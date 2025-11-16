b9195a4c8affced9ee47658639e5af140ae2130e
yomna <yomnaali463@gmail.com>
2025-11-16 09:18:00 +0200

    order status

--- lang/ar/orders.php
+++ lang/ar/orders.php
@@ -1,13 +1,17 @@
 <?php
 return [
     'status' => [
-        'pending' => 'قيد الانتظار',
-        'confirmed' => 'تم التأكيد',
-        'prepared' => 'تم التجهيز',
-        'shipped' => 'تم الشحن',
-        'delivered' => 'تم التسليم',
-        'refunded' => 'تم الاسترجاع',
-
-    ], 'payment_failed_message' => 'حدث خطأ ما',
+        'pending'             => 'قيد الانتظار',
+        'confirmed'           => 'تم التأكيد',
+        'prepared'            => 'تم التجهيز',
+        'requested_pickup'    => 'طلب الاستلام',
+        'shipped'             => 'تم الشحن',
+        'out_for_delivery'    => 'خارج للتسليم',
+        'in_transit'          => 'قيد النقل',
+        'delivery_attempted'  => 'تمت محاولة التسليم',
+        'delivered'           => 'تم التسليم',
+        'refunded'            => 'تم الاسترجاع',
+    ],
+    'payment_failed_message' => 'حدث خطأ ما',
     'payment_failed_error' => 'فشل في عملية الدفع، الرجاء المحاولة مرة أخرى لاحقًا.',
 ];
--- lang/en/orders.php
+++ lang/en/orders.php
@@ -1,13 +1,18 @@
 <?php
 return [
     'status' => [
-        'pending' => 'Pending',
-        'confirmed' => 'Confirmed',
-        'prepared' => 'Prepared',
-        'shipped' => 'Shipped',
-        'delivered' => 'Delivered',
-        'refunded' => 'Refunded',
+        'pending'             => 'Pending',
+        'confirmed'           => 'Confirmed',
+        'prepared'            => 'Prepared',
+        'requested_pickup'    => 'Requested Pickup',
+        'shipped'             => 'Shipped',
+        'out_for_delivery'    => 'Out for Delivery',
+        'in_transit'          => 'In Transit',
+        'delivery_attempted'  => 'Delivery Attempted',
+        'delivered'           => 'Delivered',
+        'refunded'            => 'Refunded',
     ],
+
     'payment_failed_message' => 'Something went wrong',
     'payment_failed_error' => 'Failed to process payment, please try again later.',
 ];
