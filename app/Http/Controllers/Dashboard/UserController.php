fc128bc9e73c1c9c8cb3f172dbd6054d2417b4fc
yomna <yomnaali463@gmail.com>
2025-11-27 11:48:27 +0200

    Update UserController.php

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -77,7 +77,6 @@ class UserController extends DashboardController
             'numbers.*' => ['exists:users,phone_number'],
             'message' => ['required', 'string'],
         ]);
-        dd("FSDfsd");
         $this->userService->sendSms($validatedData);
         return Response::api();
 
