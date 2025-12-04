f95bcadf6c4abedc92ce4c4181b964fa5c8c2cbb
yomna <yomnaali463@gmail.com>
2025-12-04 14:33:46 +0200

    Update template-modal.blade.php

--- resources/views/modals/templates/template-modal.blade.php
+++ resources/views/modals/templates/template-modal.blade.php
@@ -1,16 +1,39 @@
-<div class="modal new-user-modal fade" id="templateModal">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="add-new-user modal-content pt-0 px-1">
+<div class="modal modal-slide-in new-user-modal fade" id="templateModal">
+    <div class="modal-dialog">
+        <div class="add-new-user modal-content pt-0">
 
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-            <div class="modal-header mb-1 border-0 p-0">
-                <h5 class="modal-title fs-4">Select template to add Mockup Editor</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
+                <div class="modal-header mb-1">
+                    <h5 class="modal-title" id="exampleModalLabel">Select template to add Mockup Editor</h5>
+                </div>
+                <div class="modal-body flex-grow-1">
 
-            </div>
+                    <!-- Name in Arabic and English -->
+                    <div class="row mb-1">
+                        <div class="col-md-6">
+                            <label class="form-label label-text">Name (EN)</label>
+                            <input type="text" class="form-control" placeholder="Enter Industry Name(En)"
+                                   id="add-category-name-en" name="name[en]" />
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label label-text">Name (AR)</label>
+                            <input type="text" class="form-control" placeholder="Enter Industry Name(Ar)"
+                                   id="add-category-name-ar" name="name[ar]" />
+                        </div>
+                    </div>
 
 
+                </div>
+
+                <div class="modal-footer border-top-0">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary fs-5 saveChangesButton" id="SaveChangesButton">
+                        <span class="btn-text">Save</span>
+                        <span id="saveLoader" class="spinner-border spinner-border-sm d-none saveLoader" role="status"
+                              aria-hidden="true"></span>
+                    </button>
+                </div>
 
         </div>
-
     </div>
 </div>
