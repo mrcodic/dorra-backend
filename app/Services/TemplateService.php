0113695eafac4eb49989afa9f3399fc8abbe52d2
yomna <yomnaali463@gmail.com>
2025-12-07 11:26:35 +0200

    Update TemplateService.php

--- app/Services/TemplateService.php
+++ app/Services/TemplateService.php
@@ -64,7 +64,6 @@ class TemplateService extends BaseService
                     $q->whereRaw('1 = 0');
                 }
             })->when(filter_var(request('has_not_mockups'), FILTER_VALIDATE_BOOLEAN), function ($q) {
-                dd($q->whereDoesntHave('mockups')->get());
                 $q->whereDoesntHave('mockups');
             })
             ->when(request()->filled('types'), function ($query) {
