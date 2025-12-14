b093e5363059034645764f35f2ed1063101dd69a
yomna ali <yomnaali463@gmail.com>
2025-12-14 21:18:57 +0200

    mockups

--- app/Http/Requests/Mockup/UpdateMockupRequest.php
+++ app/Http/Requests/Mockup/UpdateMockupRequest.php
@@ -59,6 +59,8 @@ class UpdateMockupRequest extends BaseRequest
             'templates.*.none_width'   => ['nullable', 'numeric', 'min:0'],
             'templates.*.none_height'  => ['nullable', 'numeric', 'min:0'],
             'templates.*.none_angle'   => ['nullable', 'numeric'],
+            'templates.*.colors'   => ['nullable', 'array'],
+
             'front_mask_image' => [
                 Rule::requiredIf(in_array(1, $types)),
                 'image',
