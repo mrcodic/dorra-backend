d79688c50ca071a545338d1d2a95d22bf7bb64cb
yomna ali <yomnaali463@gmail.com>
2025-12-18 12:43:01 +0200

    Update HandleMockupFilesJob.php

--- app/Jobs/HandleMockupFilesJob.php
+++ app/Jobs/HandleMockupFilesJob.php
@@ -156,7 +156,7 @@ class HandleMockupFilesJob implements ShouldQueue
                 ->values()
                 ->all();
 
-            $colorsToRenderForNew = $newColors->all();
+            $colorsToRenderForNew = !empty($missingForNew) ? $missingForNew : $newColors->all();
 
             $model->templates()->updateExistingPivot($templateId, [
                 'colors' => array_values(array_unique(
