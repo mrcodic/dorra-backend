ae3beee04b7fb777b8cd8b438845fbefbf10757a
Ali Emam <aliemam2264@gmail.com>
2025-12-23 16:01:25 +0200

    another create

--- resources/views/dashboard/mockups/create.blade.php
+++ resources/views/dashboard/mockups/create.blade.php
@@ -204,19 +204,17 @@
                             <input type="text" id="templateName" class="form-control" name="name"
                                 placeholder="Mockup Name">
                         </div>
-                        <div class="row">
-                            {{-- Select Product --}}
-                            <div class="form-group mb-2 col-12">
-                                <label for="productsSelect" class="label-text mb-1">Product</label>
-                                <select id="productsSelect" name="category_id" class="form-select">
-                                    <option value="" disabled selected>Choose product</option>
-                                    @foreach($associatedData['products'] as $product)
-                                    <option value="{{ $product->id }}">
-                                        {{ $product->getTranslation('name', app()->getLocale()) }}
-                                    </option>
-                                    @endforeach
-                                </select>
-                            </div>
+                        {{-- Select Product --}}
+                        <div class="form-group mb-2 col-12">
+                            <label for="productsSelect" class="label-text mb-1">Product</label>
+                            <select id="productsSelect" name="category_id" class="form-select">
+                                <option value="" disabled selected>Choose product</option>
+                                @foreach($associatedData['products'] as $product)
+                                <option value="{{ $product->id }}">
+                                    {{ $product->getTranslation('name', app()->getLocale()) }}
+                                </option>
+                                @endforeach
+                            </select>
                         </div>
 
                         {{-- Mockup type --}}
