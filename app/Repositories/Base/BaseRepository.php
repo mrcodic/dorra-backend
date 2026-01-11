94b7094486d6da9913ae65ce6dd91ba1c1f185a6
yomna <yomnaali463@gmail.com>
2026-01-11 12:05:40 +0200

    Update BaseRepository.php

--- app/Repositories/Base/BaseRepository.php
+++ app/Repositories/Base/BaseRepository.php
@@ -41,6 +41,7 @@ class BaseRepository implements BaseRepositoryInterface
     public function update(array $data, $id)
     {
         $record = $this->model->findOrFail($id);
+        dd($record);
         $record->update($data);
         return $record;
     }
