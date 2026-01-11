6d663d8e1212a992e8353720867e924cd582b5b1
yomna <yomnaali463@gmail.com>
2026-01-11 11:41:48 +0200

    Update StoreDesignRequest.php

--- app/Http/Requests/Design/StoreDesignRequest.php
+++ app/Http/Requests/Design/StoreDesignRequest.php
@@ -170,7 +170,7 @@ class StoreDesignRequest extends BaseRequest
             'designable_id' => $this->product_id,
             'design_data' => $template?->design_data ?? $this->input('design_data'),
             'design_back_data' => $template?->design_back_data ?? $this->input('design_back_data'),
-            'price' => $template->price,
+            'price' => $template?->price,
             'name' =>$template
                 ? [
                 'en' => $template->getTranslation('name', 'en'),
