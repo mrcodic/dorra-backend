15b3720d72345e65e0e7a62ff63819782dc29f39
yomna <yomnaali463@gmail.com>
2026-01-11 15:17:37 +0200

    Update OrderController.php

--- app/Http/Controllers/Api/V1/User/Order/OrderController.php
+++ app/Http/Controllers/Api/V1/User/Order/OrderController.php
@@ -157,6 +157,7 @@ class OrderController extends Controller
 
         // Path to conversion
         $path = $media->getPath($conversion);
+        dd($path);
 
         if (! file_exists($path)) {
             abort(404, "$side {$format} conversion not found.");
