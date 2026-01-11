8a9fcfe08abc0f43b682493a3f2b75eda35cb98e
yomna <yomnaali463@gmail.com>
2026-01-11 14:44:08 +0200

    Update DesignController.php

--- app/Http/Controllers/Api/V1/User/Design/DesignController.php
+++ app/Http/Controllers/Api/V1/User/Design/DesignController.php
@@ -64,7 +64,7 @@ class DesignController extends Controller
 
     public function show($design)
     {
-        $design = $this->designService->showResource($design, ['media', 'designable','designable.prices','designable.specifications.options','dimension','template.types']);
+        $design = $this->designService->showResource($design, ['media', 'designable','designable.prices','designable.specifications.options','dimension','template.types','specifications']);
         return Response::api(data: DesignResource::make($design));
     }
 
