850c7a6971d71f37a0fb5377fa48aa77b18b644b
yomna <yomnaali463@gmail.com>
2026-01-11 13:55:56 +0200

    Update StoreCartItemRequest.php

--- app/Http/Requests/User/Cart/StoreCartItemRequest.php
+++ app/Http/Requests/User/Cart/StoreCartItemRequest.php
@@ -82,6 +82,7 @@ class StoreCartItemRequest extends BaseRequest
         $cartable     = $this->cartable();
         $hasSpecs     = (bool) $cartable?->specifications()->exists();
         $type = (int) $this->input('type', TypeEnum::PRINT->value);
+        dd($this->design->designable);
         return [
             'type' =>['sometimes',Rule::in(TypeEnum::values())],
             'design_id' => ['required_without:template_id', 'string', 'exists:designs,id'],
