d50def7acad4d46ff494ccd27e97946787389857
yomna <yomnaali463@gmail.com>
2026-02-04 10:09:25 +0200

    Update ProductController.php

--- app/Http/Controllers/Api/V1/User/Product/ProductController.php
+++ app/Http/Controllers/Api/V1/User/Product/ProductController.php
@@ -34,7 +34,7 @@ class ProductController extends Controller
     public function show(Product $product, Request $request)
     {
         return Response::api(data: ProductResource::make($this->productService->showResource($product->id, [
-            'category:id,name',
+            'category:id,name,is_has_category',
             'templates.industries.children',
             'media',
             'specifications.options',
