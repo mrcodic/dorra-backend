9ae5085d0f792571485ed6968117714ce17fba24
yomna <yomnaali463@gmail.com>
2026-02-09 10:31:32 +0200

    Update CreditOrder.php

--- app/Models/CreditOrder.php
+++ app/Models/CreditOrder.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\CreditOrder\StatusEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CreditOrder extends Model
 {
@@ -16,18 +17,29 @@ class CreditOrder extends Model
         'plan_id'
     ];
 
-    protected function casts()
+    protected static function booted(): void
+    {
+        self::created(function ($model) {
+            $model->number = sprintf('%s-%s-%06d', "#CRORD", now()->format('Ymd'), $model->id);
+            $model->saveQuietly();
+        });
+        parent::booted();
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
     {
         return [
             'status' => StatusEnum::class,
         ];
-    }
-    protected static function booted()
-    {
-        self::created(function ($model) {
-            $model->number = sprintf('%s-%s-%06d',"#CRORD", now()->format('Ymd'), $model->id);
-            $model->saveQuietly();
-        });
-        parent::booted();
     }
 }
