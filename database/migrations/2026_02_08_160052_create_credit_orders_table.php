64bb0c493b0a7032d2549c42fe5e085beed06d34
yomna <yomnaali463@gmail.com>
2026-02-09 10:28:03 +0200

    Update 2026_02_08_160052_create_credit_orders_table.php

--- database/migrations/2026_02_08_160052_create_credit_orders_table.php
+++ database/migrations/2026_02_08_160052_create_credit_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('credit_orders', function (Blueprint $table) {
             $table->id();
-            $table->string('number')->unique();
+            $table->string('number')->unique()->nullable();
             $table->foreignIdFor(User::class)
                 ->nullable()
                 ->constrained()
