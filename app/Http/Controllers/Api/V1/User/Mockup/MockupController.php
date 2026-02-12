3b8595c192727e5dba275b8b889a79e929f647ec
yomna <yomnaali463@gmail.com>
2026-02-12 10:10:47 +0200

    Update MockupController.php

--- app/Http/Controllers/Api/V1/User/Mockup/MockupController.php
+++ app/Http/Controllers/Api/V1/User/Mockup/MockupController.php
@@ -19,7 +19,7 @@ class MockupController extends Controller
     public function index()
     {
         $data = $this->mockupService->getAll(['types'], true, perPage: 10);
-        dd($data);
+        dd($data, MockupResource::collection($data->load('types'))->response()->getData(true));
         return Response::api(data: MockupResource::collection($data->load('types'))->response()->getData(true));
 
     }
