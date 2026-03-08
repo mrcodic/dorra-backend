9ff94ae369cb08afd3c3d8406a9ba6d6d3b6b10d
yomna ali <yomnaali463@gmail.com>
2026-03-08 12:03:35 +0200

    Update FontController.php

--- app/Http/Controllers/Dashboard/FontController.php
+++ app/Http/Controllers/Dashboard/FontController.php
@@ -24,8 +24,10 @@ class FontController extends Controller
     {
         $fonts = $this->fontService->getAll(['fontStyles.media', 'fontStyles.font'], request('paginate',false), perPage:request('per_page',10),
         counts: [
-                'fontStyles.media as fonts_count' => function ($q) {
-                    $q->wherePivot('type', 'font');
+                'fontStyles as templates_fonts_count' => function ($q) {
+                    $q->whereHas('media.templates', function ($q2) {
+                        $q2->where('mediables.type', 'font');
+                    });
                 }
             ]
         );
