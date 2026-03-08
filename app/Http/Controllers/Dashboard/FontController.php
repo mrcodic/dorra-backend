2f6d424d070c0dafe93e5bbf69eba47d5c39bcf5
yomna ali <yomnaali463@gmail.com>
2026-03-08 12:07:23 +0200

    Update FontController.php

--- app/Http/Controllers/Dashboard/FontController.php
+++ app/Http/Controllers/Dashboard/FontController.php
@@ -25,7 +25,7 @@ class FontController extends Controller
         $fonts = $this->fontService->getAll(['fontStyles.media', 'fontStyles.font'], request('paginate',false), perPage:request('per_page',10),
         counts: [
                 'fontStyles as templates_fonts_count' => function ($q) {
-                    $q->whereHas('libraryMedia.templates', function ($q2) {
+                    $q->whereHas('libraryMedia', function ($q2) {
                         $q2->where('mediables.type', 'font');
                     });
                 }
