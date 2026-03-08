21671a815e68e27509b9719bc2d39b372f538943
yomna ali <yomnaali463@gmail.com>
2026-03-08 13:35:21 +0200

    Update TemplateResource.php

--- app/Http/Resources/Template/TemplateResource.php
+++ app/Http/Resources/Template/TemplateResource.php
@@ -55,7 +55,7 @@ class TemplateResource extends JsonResource
             'has_corner' => $this->has_corner,
             'has_safety_area' => $this->has_safety_area,
             'safety_area' => $this->safety_area,
-            'border' => (float)$this->border,
+            'border' => $this->border,
             'has_cut_margin' => (bool)$this->cut_margin,
             'cut_margin' => $this->cut_margin,
             'approach' => $this->approach,
