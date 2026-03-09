142848b8a30fb8e58dc778df4951970be8542ca4
yomna ali <yomnaali463@gmail.com>
2026-03-09 13:49:37 +0200

    Update StoreCartItemRequest.php

--- app/Http/Requests/User/Cart/StoreCartItemRequest.php
+++ app/Http/Requests/User/Cart/StoreCartItemRequest.php
@@ -126,7 +126,7 @@ class StoreCartItemRequest extends BaseRequest
                 'integer',
                 'exists:product_specification_options,id',
             ],
-            'color' => ['sometimes', 'string'],
+            'color' => ['nullable', 'string'],
         ];
     }
 
