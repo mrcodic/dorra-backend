b973e8225bcd93a893da68fb883b4de2b94f4e64
yomna ali <yomnaali463@gmail.com>
2026-03-10 11:02:04 +0200

    Update DesignService.php

--- app/Services/DesignService.php
+++ app/Services/DesignService.php
@@ -82,7 +82,7 @@ class DesignService extends BaseService
 
         } else {
             $design = $this->repository->query()->create($validatedData);
-            $design->types()->attach([3]);
+            $design->types()->attach([1]);
         }
         if ($validatedData['user_id']) {
             $design->users()->attach(
