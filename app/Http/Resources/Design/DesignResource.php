fc51c26ddad9db4c6c117c368800413aaae5202a
yomna ali <yomnaali463@gmail.com>
2026-03-10 15:29:50 +0200

    Update DesignResource.php

--- app/Http/Resources/Design/DesignResource.php
+++ app/Http/Resources/Design/DesignResource.php
@@ -31,8 +31,8 @@ class DesignResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'description' => $this->description,
+            'name'=> $this->name,
+            'description'=> $this->description,
             'design_data' => $this->when(request('design_data') == true, $this->design_data),
             'design_back_data' => $this->when(request('design_data') == true, $this->design_back_data),
             'design_image' => $this->getFirstMediaUrl('designs'),
@@ -41,15 +41,15 @@ class DesignResource extends JsonResource
             'types' => TypeResource::collection($this->whenLoaded('types')),
             'mockup_design_image' =>
                 $this->getFirstMediaUrl('front-mockup-designs') ?:
-                    $this->getFirstMediaUrl('none-mockup-designs') ?:
-                        $this->getFirstMediaUrl('back-mockup-designs')
+                $this->getFirstMediaUrl('none-mockup-designs') ?:
+                    $this->getFirstMediaUrl('back-mockup-designs')
             ,
             'current_version' => $this->current_version,
             'product' => $this->when($designable, function () use ($designable) {
-                return new CategoryResource($designable instanceof Category ? $designable : $designable->category);
+                return  new CategoryResource($designable instanceof Category ? $designable : $designable->category);
             }),
-            'designable' => $this->when($designable, function () use ($designable) {
-                return $designable instanceof Category ? CategoryResource::make($designable) : ProductResource::make($designable);
+            'designable'=> $this->when($designable, function () use ($designable) {
+                return  $designable instanceof Category ? CategoryResource::make($designable) : ProductResource::make($designable);
             }),
             'selected_category' => $this->when(
                 $designable instanceof Product,
@@ -65,8 +65,8 @@ class DesignResource extends JsonResource
             'specs' => $this->whenLoaded('specifications', function () {
                 return $this->specifications->map(function ($spec) {
                     return [
-                        'id' => $spec->id,
-                        'name' => $spec->name,
+                        'id'     => $spec->id,
+                        'name'   => $spec->name,
                         'option' => ProductSpecificationOption::make($spec->pivot->option),
                         'options' => ProductSpecificationOption::collection($spec->options),
                     ];
@@ -79,62 +79,96 @@ class DesignResource extends JsonResource
             'ownered_by_me' => $this->owner?->id == auth('sanctum')->user()?->id,
             'type' => 'design',
             'delete_since' => $this->deleted_at?->diffForHumans(),
-            'is_saved' => $this->when(!$this->deleted_at, fn() => $this->saves->contains(fn($save) => $save->user_id === auth('sanctum')->id()),),
-            'orientation' => [
+            'is_saved' => $this->when(!$this->deleted_at,fn() => $this->saves->contains(fn($save) => $save->user_id === auth('sanctum')->id()),),
+            'orientation' =>  [
                 'value' => $this->orientation?->value,
                 'label' => $this->orientation?->label(),
             ],
             'design_price' => $this->price,
-            'visible_download_btn' => (bool)$this->price,
-            'linked_to_mockup' => $this->linked_to_mockup,
-            'mockup_color' => $this->mockup_color,
-            'design_mockup_area' => $this->design_mockup_area ??
-                $this->types->map(function ($type) {
-
-                    if ($type->value->key() === 'front') {
-                        return [
-                            "name" => "front",
-                            "p1x" => "0.350000",
-                            "p1y" => "0.250000",
-                            "p2x" => "0.650000",
-                            "p2y" => "0.250000",
-                            "p3x" => "0.350000",
-                            "p3y" => "0.550000",
-                            "p4x" => "0.650000",
-                            "p4y" => "0.550000"
-                        ];
-                    }
-
-                    if ($type->value->key() === 'back') {
-                        return [
-                            "name" => "back",
-                            "p1x" => "0.350000",
-                            "p1y" => "0.250000",
-                            "p2x" => "0.650000",
-                            "p2y" => "0.250000",
-                            "p3x" => "0.350000",
-                            "p3y" => "0.550000",
-                            "p4x" => "0.650000",
-                            "p4y" => "0.550000"
-                        ];
-                    }
-
-                    if ($type->value->key() === 'none') {
-                        return [
-                            "name" => "none",
-                            "p1x" => "0.350000",
-                            "p1y" => "0.250000",
-                            "p2x" => "0.650000",
-                            "p2y" => "0.250000",
-                            "p3x" => "0.350000",
-                            "p3y" => "0.550000",
-                            "p4x" => "0.650000",
-                            "p4y" => "0.550000"
-                        ];
-                    }
-
-                })->values(),
-
+            'visible_download_btn' => (bool) $this->price,
+            'linked_to_mockup' =>  $this->linked_to_mockup,
+            'mockup_color' =>  $this->mockup_color,
+            'design_mockup_area' =>  $this->design_mockup_area ??  [
+                    [
+                        "name" => "front",
+                        "p1x" => "0.350000",
+                        "p1y" => "0.250000",
+                        "p2x" => "0.650000",
+                        "p2y" => "0.250000",
+                        "p3x" => "0.350000",
+                        "p3y" => "0.550000",
+                        "p4x" => "0.650000",
+                        "p4y" => "0.550000"
+                    ],
+                    [
+                        "name" => "back",
+                        "p1x" => "0.350000",
+                        "p1y" => "0.250000",
+                        "p2x" => "0.650000",
+                        "p2y" => "0.250000",
+                        "p3x" => "0.350000",
+                        "p3y" => "0.550000",
+                        "p4x" => "0.650000",
+                        "p4y" => "0.550000"
+                    ],
+                    [
+                        "name" => "none",
+                        "p1x" => "0.350000",
+                        "p1y" => "0.250000",
+                        "p2x" => "0.650000",
+                        "p2y" => "0.250000",
+                        "p3x" => "0.350000",
+                        "p3y" => "0.550000",
+                        "p4x" => "0.650000",
+                        "p4y" => "0.550000"
+                    ],
+                ],
+//            'design_mockup_area' => $this->design_mockup_area ??
+//                $this->types->map(function ($type) {
+//
+//                    if ($type->value->key() === 'front') {
+//                        return [
+//                            "name" => "front",
+//                            "p1x" => "0.350000",
+//                            "p1y" => "0.250000",
+//                            "p2x" => "0.650000",
+//                            "p2y" => "0.250000",
+//                            "p3x" => "0.350000",
+//                            "p3y" => "0.550000",
+//                            "p4x" => "0.650000",
+//                            "p4y" => "0.550000"
+//                        ];
+//                    }
+//
+//                    if ($type->value->key() === 'back') {
+//                        return [
+//                            "name" => "back",
+//                            "p1x" => "0.350000",
+//                            "p1y" => "0.250000",
+//                            "p2x" => "0.650000",
+//                            "p2y" => "0.250000",
+//                            "p3x" => "0.350000",
+//                            "p3y" => "0.550000",
+//                            "p4x" => "0.650000",
+//                            "p4y" => "0.550000"
+//                        ];
+//                    }
+//
+//                    if ($type->value->key() === 'none') {
+//                        return [
+//                            "name" => "none",
+//                            "p1x" => "0.350000",
+//                            "p1y" => "0.250000",
+//                            "p2x" => "0.650000",
+//                            "p2y" => "0.250000",
+//                            "p3x" => "0.350000",
+//                            "p3y" => "0.550000",
+//                            "p4x" => "0.650000",
+//                            "p4y" => "0.550000"
+//                        ];
+//                    }
+//
+//                })->values(),
             'is_added_to_cart' => $this->isAddedToCart(),
         ];
     }
