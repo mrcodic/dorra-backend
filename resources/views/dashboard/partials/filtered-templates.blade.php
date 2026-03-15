77282499bbf24e7964fcdc0f0197e0b74faa69fc
yomna ali <yomnaali463@gmail.com>
2026-03-15 13:23:56 +0200

    Update filtered-templates.blade.php

--- resources/views/dashboard/partials/filtered-templates.blade.php
+++ resources/views/dashboard/partials/filtered-templates.blade.php
@@ -142,8 +142,8 @@
             </div> <!-- Tags -->
             <div class="mb-1 d-flex flex-wrap gap-1">
                 <p style="display: inline">Created
-                    at: {{ $template->created_at->format('d/m/Y h:i A') }}</p>
-                <p style="display: inline">Last update: {{ $template->updated_at->format('d/m/Y h:i A') }}</p>
+                    at: {{ $template->created_at->timezone('Africa/Cairo')->format('d/m/Y h:i A') }}</p>
+                <p style="display: inline">Last update: {{ $template->updated_at->timezone('Africa/Cairo')->format('d/m/Y h:i A') }}</p>
             </div>
             <div class="d-flex flex-wrap justify-content-start gap-1 mb-1" style="min-height: 44px;">
                 @foreach($template->tags as $tag)
