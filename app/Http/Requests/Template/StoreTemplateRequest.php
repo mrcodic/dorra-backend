e3e709c925f90027690a577c683c458ad93de1a0
yomna ali <yomnaali463@gmail.com>
2026-03-18 10:14:54 +0200

    Update StoreTemplateRequest.php

--- app/Http/Requests/Template/StoreTemplateRequest.php
+++ app/Http/Requests/Template/StoreTemplateRequest.php
@@ -48,16 +48,19 @@ class StoreTemplateRequest extends BaseRequest
             'types.*' => ['integer', 'exists:types,id'],
             'template_image_front_id' => [
                 Rule::requiredIf(fn()=> in_array(TypeEnum::FRONT->value, (array)request('types', []), )),
+                'nullable',
                 'exists:media,id',
             ],
 
             'template_image_back_id' => [
                 Rule::requiredIf(fn()=> in_array(TypeEnum::BACK->value, (array)request('types', []), )),
+                'nullable',
                 'exists:media,id',
             ],
 
             'template_image_none_id' => [
                 Rule::requiredIf(fn()=> in_array(TypeEnum::NONE->value, (array)request('types', []), )),
+                'nullable',
                 'exists:media,id',
             ],
             'template_image_id' => ['nullable','exists:media,id'],
