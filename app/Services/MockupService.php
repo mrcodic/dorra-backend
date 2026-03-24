aedb666661958531655920c93bf30bd7b196adf0
yomna <yomnaali463@gmail.com>
2026-03-24 16:49:47 +0200

    Update MockupService.php

--- app/Services/MockupService.php
+++ app/Services/MockupService.php
@@ -267,11 +267,11 @@ class MockupService extends BaseService
         $this->handleFiles($model);
         $model->load(['templates', 'types', 'category', 'media']);
         if (!empty($validatedData['templates'])) {
-            $templateIds = collect($validatedData['templates'])->pluck('template_id')->filter();
-            $templates = Template::whereIn('id', $templateIds)->get();
-
-            $this->renderMockupsForTemplates($templates, 'templates');
-            $this->renderMockupsForTemplates($templates, 'back_templates');
+//            $templateIds = collect($validatedData['templates'])->pluck('template_id')->filter();
+//            $templates = Template::whereIn('id', $templateIds)->get();
+//
+//            $this->renderMockupsForTemplates($templates, 'templates');
+//            $this->renderMockupsForTemplates($templates, 'back_templates');
 
             HandleMockupFilesJob::dispatch($model, 'create')->delay(now()->addSeconds(5));
         }
@@ -396,10 +396,10 @@ class MockupService extends BaseService
 
             $model->load(['templates', 'types', 'category', 'media']);
             if(!empty($validatedData['templates'])){
-                $templates = Template::whereIn('id', $validatedData['templates'])->get();
-
-                $this->renderMockupsForTemplates($templates, 'templates');
-                $this->renderMockupsForTemplates($templates, 'back_templates');
+//                $templates = Template::whereIn('id', $validatedData['templates'])->get();
+//
+//                $this->renderMockupsForTemplates($templates, 'templates');
+//                $this->renderMockupsForTemplates($templates, 'back_templates');
                 HandleMockupFilesJob::dispatch($model)->delay(now()->addSeconds(5));
             }
 
