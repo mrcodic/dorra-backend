0f4438dd953f7f00b270366183d4646a8886aba2
yomna <yomnaali463@gmail.com>
2026-04-01 09:52:53 +0200

    templates

--- resources/views/dashboard/templates/create.blade.php
+++ resources/views/dashboard/templates/create.blade.php
@@ -1436,6 +1436,8 @@ $category = \App\Models\Category::find(request('category_id'));
                 // ✅ 3) Default fallback
                 setTimeout(function () {
                     const params = new URLSearchParams(window.location.search);
+                    params.set('product_without_category_id', params.get('category_id'));
+                    params.delete('category_id');
                     window.location.href = '/product-templates?' + params.toString();
                 }, 1000);
             },
--- resources/views/dashboard/templates/edit.blade.php
+++ resources/views/dashboard/templates/edit.blade.php
@@ -1839,7 +1839,8 @@
 
                 }
                 setTimeout(function () {
-                    const params = new URLSearchParams(window.location.search);
+                    params.set('product_without_category_id', params.get('category_id'));
+                    params.delete('category_id');
                     window.location.href = '/product-templates?' + params.toString();
                 }, 1000);
             }
