daf53a9cc9356b2db5f18a8670dda8b2dad35d86
yomna <yomnaali463@gmail.com>
2026-04-05 16:57:55 +0200

    Update CartService.php

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -115,7 +115,7 @@ class CartService extends BaseService
         $basePrice = $product->base_price ?? $productPriceValue;
 
         $subTotal = ($product->base_price ?? $productPriceValue) + $specsSum;
-        dd($subTotal,$product->base_price,$specsSum);
+        dd($subTotal,$product->base_price,$specsSum,($product->base_price ?? $productPriceValue) + $specsSum);
         $calculatePrices = [
             'product_price' => $basePrice,
             'specs_sum' => $specsSum,
