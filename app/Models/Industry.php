830f13fc0ce3d691cc562e92f1a0248a1c0c60ac
yomna <yomnaali463@gmail.com>
2026-04-05 11:34:55 +0200

    Update Industry.php

--- app/Models/Industry.php
+++ app/Models/Industry.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Translatable\HasTranslations;
 
 class Industry extends Model
