1c17b3f4efc6c186e1d77354eb87bd98471868d3
yomna <yomnaali463@gmail.com>
2026-04-07 09:45:52 +0200

    Update ValidDiscountCode.php

--- app/Rules/ValidDiscountCode.php
+++ app/Rules/ValidDiscountCode.php
@@ -36,7 +36,7 @@ class ValidDiscountCode implements ValidationRule
             $fail('This discount code is no longer valid.');
             return;
         }
-dd($code?->products->contains($this->product),$this->product,$this->category,$code?->categories->contains($this->category));
+dd($code?->products->contains($this->product),$this->product,$this->category,$code?->categories->contains($this->category),$code?->products);
         if (!$code?->products->contains($this->product) && !$code?->categories->contains($this->category) && $code?->scope != ScopeEnum::GENERAL) {
             $fail('This discount code is not valid for the selected product or category.');
         }
