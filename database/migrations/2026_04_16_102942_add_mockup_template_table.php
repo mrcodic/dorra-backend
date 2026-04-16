8fdd9cc933817b048fcd265ae9bf91b7eb129a30
yomna <yomnaali463@gmail.com>
2026-04-16 10:39:13 +0200

    Update 2026_04_16_102942_add_mockup_template_table.php

--- database/migrations/2026_04_16_102942_add_mockup_template_table.php
+++ database/migrations/2026_04_16_102942_add_mockup_template_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('designs', function (Blueprint $table) {
-            $table->string('model_color');
+            $table->string('model_color')->nullable();
         });
     }
 
