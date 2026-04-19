17ca5b812ae56f808e9cc9e0d43acd6885881c99
yomna <yomnaali463@gmail.com>
2026-04-19 11:12:10 +0200

    Update TemplateController.php

--- app/Http/Controllers/Dashboard/TemplateController.php
+++ app/Http/Controllers/Dashboard/TemplateController.php
@@ -422,7 +422,7 @@ class TemplateController extends DashboardController
             $side = $fileData['side'] ?? 'front';
             $hex = $fileData['color'] ?? '#000000';
             $safeHex = ltrim($hex, '#');
-
+dd($mockup->templateColors);
             $mockup->getMedia('generated_mockups')
                 ->filter(fn($m) => $m->getCustomProperty('template_id') == $template->id &&
                     $m->getCustomProperty('side') == $side &&
