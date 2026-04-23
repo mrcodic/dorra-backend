a0e982c4d3536ec043cfdd59a0599d3f40632822
yomna <yomnaali463@gmail.com>
2026-04-23 15:04:44 +0200

    Update FawryStrategy.php

--- app/Services/Payment/FawryStrategy.php
+++ app/Services/Payment/FawryStrategy.php
@@ -202,7 +202,7 @@ use Illuminate\Support\Facades\Log;
                     'signature'      => $signature,
                 ]
             );
-            dd($response->json());
+            dd($response);
             return $response->json('paymentStatus') ?? 'UNKNOWN';
 
         } catch (\Exception $e) {
