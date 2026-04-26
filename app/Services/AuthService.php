a8b92e9f25e5f789e3a6a9343f1c6728d9dcc097
yomna <yomnaali463@gmail.com>
2026-04-26 13:42:16 +0300

    Update AuthService.php

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -291,19 +291,23 @@ class AuthService
                 });
 
 
-
+                if ($guestCart) {
+                    $guestCart->items()->delete();
+                    $guestCart->delete();
+                }
             } else {
 
                 $this->cartRepository->query()
                     ->whereNull('user_id')
                     ->where('guest_id', $guest->id)
                     ->update(['user_id' => $user->id]);
+                if ($guestCart) {
+                    $guestCart->items()->delete();
+                    $guestCart->delete();
+                }
             }
 
-            if ($guestCart) {
-                $guestCart->items()->delete();
-                $guestCart->delete();
-            }
+
             $this->designRepository->query()
                 ->whereNull('user_id')
                 ->where('guest_id', $guest->id)
