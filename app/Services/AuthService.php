aff49076a1e348c49ceeefd96a070b6698a54749
yomna <yomnaali463@gmail.com>
2026-04-26 16:21:13 +0300

    Update AuthService.php

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -66,7 +66,7 @@ class AuthService
     public function redirectToGoogle(Request $request)
     {
         $cookieId = $request->cookie('cookie_id') ?: $request->cookie('dorra_auth_cookie_id') ?: (string) Str::uuid();
-        dd($cookieId);
+        dd($request->cookie('cookie_id'),$request->cookie('dorra_auth_cookie_id'));
 
         Cookie::queue(cookie(
             name: 'dorra_auth_cookie_id',
