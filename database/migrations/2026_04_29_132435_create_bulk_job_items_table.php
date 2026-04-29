8fad6e7492f5dcad70d9e1611a978a869560ee7b
yomna <yomnaali463@gmail.com>
2026-04-29 14:46:07 +0300

    Update 2026_04_29_132435_create_bulk_job_items_table.php

--- database/migrations/2026_04_29_132435_create_bulk_job_items_table.php
+++ database/migrations/2026_04_29_132435_create_bulk_job_items_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\MockupGenerationJob;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +11,8 @@ return new class extends Migration
         Schema::create('bulk_job_items', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignIdFor(MockupGenerationJob::class)
-                ->constrained()
+            $table->foreignId('bulk_job_id')
+                ->constrained('mockup_generation_jobs')
                 ->cascadeOnDelete();
 
             $table->foreignId('template_id')
