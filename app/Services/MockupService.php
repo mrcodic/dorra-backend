76a066ca72ba0f8907ee8733bd3cadff87d739df
yomna <yomnaali463@gmail.com>
2026-05-04 10:24:48 +0300

    Update MockupService.php

--- app/Services/MockupService.php
+++ app/Services/MockupService.php
@@ -171,6 +171,7 @@ class MockupService extends BaseService
                 $productIds = request('product_ids');
 
                 if ($type === 'category') {
+                    dd($productIds);
                     $q->whereIn('category_id',$productIds);
 //                    $q->whereCategoryId($productId);
                 }
