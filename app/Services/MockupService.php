5e4c270da0886fdd7c1cdc9e12b38cb0c1b988cd
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-05-05 12:08:14 +0300

    Update MockupService.php

--- app/Services/MockupService.php
+++ app/Services/MockupService.php
@@ -173,7 +173,7 @@ class MockupService extends BaseService
 
         $query = $this->repository
             ->query()
-            ->with(['category:id,name', 'types'])
+            ->with(['category:id,name,is_has_category', 'types'])
             ->when(request()->filled('search_value'), function ($q) {
                 if (hasMeaningfulSearch(request('search_value'))) {
                     $q->where('name', 'LIKE', '%' . request('search_value') . '%');
