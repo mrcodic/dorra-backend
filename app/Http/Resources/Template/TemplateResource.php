0388fec14649db13e523c114785ca4a4686f20b4
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-05-13 16:58:00 +0300

    Update TemplateResource.php

--- app/Http/Resources/Template/TemplateResource.php
+++ app/Http/Resources/Template/TemplateResource.php
@@ -82,7 +82,7 @@ class TemplateResource extends JsonResource
             })->values()->all()
             ),
             'show_back' => $this->types->contains(TypeEnum::BACK) && (
-                    !$media || $media->getCustomProperty('side') === 'back'
+                    !$media || $media->getCustomProperty('side') === 'back' ?? true
                 ),
             'source_design_svg' => $this->when(isset($this->image), $this->image),
             'back_base64_preview_image' => $this->use_front_as_back
