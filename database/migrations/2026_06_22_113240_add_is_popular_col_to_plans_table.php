12311f5c7f41891860d860df2d7eb5c4dfd82d1f
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-06-22 15:00:21 +0300

    Update 2026_06_22_113240_add_is_popular_col_to_plans_table.php

--- database/migrations/2026_06_22_113240_add_is_popular_col_to_plans_table.php
+++ database/migrations/2026_06_22_113240_add_is_popular_col_to_plans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->boolean('is_popular')->default(false);
+            $table->boolean('is_popular')->after('is_active')->default(false);
         });
     }
 
