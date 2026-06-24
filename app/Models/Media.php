822fc535cf1f36ed5021472d8faeb1bc361f2642
yomna ali <yomnaali463@gmail.com>
2026-06-24 15:10:59 +0300

    Update Media.php

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -7,6 +7,24 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Media extends \Spatie\MediaLibrary\MediaCollections\Models\Media
 {
+    protected static function booted()
+    {
+        static::deleting(function ($media) {
+            $previewId = $media->getCustomProperty('preview_id');
+
+            if (!$previewId) {
+                return;
+            }
+
+            $preview = Media::find($previewId);
+            
+            if ($preview && $preview->id !== $media->id) {
+                $preview->delete();
+            }
+        });
+        parent::booted();
+    }
+
     public function templates(): MorphToMany
     {
         return $this->morphedByMany(
