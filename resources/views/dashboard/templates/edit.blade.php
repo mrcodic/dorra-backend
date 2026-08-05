a880b77588382d17818da5b860fbe91b1cab852e
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-08-05 10:24:36 +0300

    Update edit.blade.php

--- resources/views/dashboard/templates/edit.blade.php
+++ resources/views/dashboard/templates/edit.blade.php
@@ -933,8 +933,7 @@
     <script>
         // Pre-selected mockup IDs from the server (already attached to this template)
         const attachedMockupIds = new Set(
-            @json(($model?->mockups?->whereNotNull('type')->pluck('id') ?? collect())->values()));
-
+            @json(($model?->mockups?->whereNotNull('pivot.type')->pluck('id') ?? collect())->values()));
         $(function () {
             const $cardsWrap = $('#mockupsCards');
             const $hiddenWrap = $('#mockupsHiddenInputs');
