5582a246bad5715cfa524deca1c0d1cf468df95b
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-10 11:31:49 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -340,7 +340,6 @@ Route::middleware(AutoCheckPermission::class)->group(function () {
             Route::post('/bulk-delete',  'bulkDelete')->name('bulk-delete');
             Route::get('/{id}/questions',  'questions')->name('questions');
             Route::put('/{id}/questions',  'updateQuestions')->name('questions.update');
-            Route::post('questions/quick-store', 'quickStoreQuestion')->name('questions.quick-store');
 
 
         }));
