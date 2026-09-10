e062ca4d5f9de6190b68a787932a02d1d14388a9
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-10 15:53:20 +0300

    Update BundleController.php

--- app/Http/Controllers/Api/V1/User/BundleController.php
+++ app/Http/Controllers/Api/V1/User/BundleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\User\Cart\StoreBundleCartRequest;
+use App\Http\Requests\Cart\StoreBundleCartRequest;
 use App\Http\Resources\Bundle\BundleResource;
 use App\Services\Bundle\BundleCartService;
 use App\Services\BundleService;
