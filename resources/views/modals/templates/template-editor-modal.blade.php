9e451a3f24ab48ad1edca880c0574923353cc676
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-16 12:40:04 +0300

    Update template-editor-modal.blade.php

--- resources/views/modals/templates/template-editor-modal.blade.php
+++ resources/views/modals/templates/template-editor-modal.blade.php
@@ -1,10 +1,7 @@
 @php
-    $categoryId = request('product_without_category_id');
-    $category = $categoryId
-        ? \Illuminate\Support\Facades\Cache::remember("category_{$categoryId}", 300, fn () => \App\Models\Category::find($categoryId))
-        : null;
+  $category = \App\Models\Category::find(request('product_without_category_id'));
     $isTableau = $category && $category->is_tableau;
-@endphp
+  @endphp
 <div class="modal new-user-modal fade" id="templateEditorModal">
     <div class="modal-dialog modal-dialog-centered">
         <div class="add-new-user modal-content pt-0 px-1">
