ce16574740ab2762b43de9a2571ea9ebf683c417
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-16 12:17:55 +0300

    Update show-bundle.blade.php

--- resources/views/modals/bundles/show-bundle.blade.php
+++ resources/views/modals/bundles/show-bundle.blade.php
@@ -33,11 +33,6 @@
 
                 <div class="row">
                     <div class="col-md-6 mb-2">
-                        <label class="label-text mb-1">Usage</label>
-                        <input id="showBundleRepeat" class="form-control" readonly>
-                    </div>
-
-                    <div class="col-md-6 mb-2">
                         <label class="label-text mb-1">Display On Visit</label>
                         <input id="showBundleDisplayOnVisit" class="form-control" readonly>
                     </div>
