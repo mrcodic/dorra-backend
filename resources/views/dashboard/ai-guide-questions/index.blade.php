2545335ca28789fdc35eb58ac9e91ddd822c1b7d
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-17 16:45:16 +0300

    Update index.blade.php

--- resources/views/dashboard/ai-guide-questions/index.blade.php
+++ resources/views/dashboard/ai-guide-questions/index.blade.php
@@ -6,6 +6,7 @@
 @section('vendor-style')
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap5.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
 @endsection
 
 @section('content')
@@ -92,6 +93,7 @@
     <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')) }}"></script>
     <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
     <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap5.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
 @endsection
 
 @section('page-script')
