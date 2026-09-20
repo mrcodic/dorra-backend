bb70b1e4c3b9b087d3964cbb513c205614633eac
Yomna Ali <101651280+YomnaAli26@users.noreply.github.com>
2026-09-20 15:36:43 +0300

    Update StoreCartItemRequest.php

--- app/Http/Requests/User/Cart/StoreCartItemRequest.php
+++ app/Http/Requests/User/Cart/StoreCartItemRequest.php
@@ -247,6 +247,7 @@ class StoreCartItemRequest extends BaseRequest
             ->when($this->product_price_id, fn($q) => $q->where('product_price_id', $this->product_price_id))
             ->when($this->template_id, fn($q) => $q->where('itemable_id', $this->template_id))
             ->when($this->design_id, fn($q) => $q->where('itemable_id', $this->design_id))
+            ->whereNull('bundle_group_key')
             ->with('specs')
             ->get();
 
